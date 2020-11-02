327ef1b566b0542da19811f649fe47d6a2fdfdfd
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2020-11-02 15:56:34 -0800

    controllers, views, and db migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '/drivers/', to: 'drivers#index', as: :driver
+  get '/passengers/', to: 'passengers#index', as: :passenger
+  get '/trips/', to: 'trips#index', as: :trip
 end
