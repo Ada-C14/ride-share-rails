7ae05f43293410e93c057cbc79af82a9945b2b3b
Tram Bui <pbui17@cmc.edu>
2020-11-02 21:32:04 -0600

    created the three controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # what should our root_path be?
+
+  resources :trips
+  resources :drivers
+  resources :passengers
+
 end
