b6560b235f008c6ba41f40030b31a53b3684143a
Tram Bui <pbui17@cmc.edu>
2020-11-03 15:20:26 -0600

    made partial view forms and updated the new and create actions

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -16,6 +16,36 @@ class PassengersController < ApplicationController
     @passenger = Passenger.new
   end
 
+  def create
+    @passenger = Passenger.new(passenger_params)
+
+    if @passenger.save
+      redirect_to passengers_path
+      return
+    else # save failed
+      render :new
+    end
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_num)
+  end
+
 
 
 
