0a2f6cd3d48a9316a9b776e262ad27bb3c2c3973
dnsrocha <drocha@gmail.com>
2020-11-03 13:09:44 -0800

    Implemented initial methods for trips controller and strong params for trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -3,4 +3,57 @@ class TripsController < ApplicationController
   def index
     @trips = Trip.all
   end
+
+  def show
+    @trip = Trip.find_by(id: params[:id])
+
+    if @trip.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def edit
+    @trip = Trip.find_by(id: params[:id])
+
+    if @trip.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+    @trip = Trip.find_by(id: params[:id])
+
+    if @trip.nil?
+      head :not_found
+      return
+    elsif @trip.update(trip_params)
+      redirect_to trip_path
+      return
+    else
+      render :edit
+      return
+    end
+
+  def destroy
+    @trip = Trip.find_by(id: params[:id])
+
+    if @trip.nil?
+      head :not_found
+      return
+    elsif @trip.destroy
+      redirect_to root_path
+      return
+    else # to-do: what are we gonna do here huh
+    render trip_path
+    return
+    end
+
+  end
+
+  private
+  def trip_params
+    return params.require(:trip).permit(:id, :driver_id, :passenger_id, :date, :rating, :cost)
+  end
 end
