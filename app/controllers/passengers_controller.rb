13fe0153de6cd47f2ef344a844fbc7eb3523fae8
Li Dai <li.lea.dai@gmail.com>
2020-11-03 15:28:35 -0800

    Created actions create, edit, update and destory for passenger controllers.

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -18,9 +18,55 @@ class PassengersController < ApplicationController
   end
 
   def create
-    @passenger = Passenger.new(
-        id: params[]
-    )
+    @passenger = Passenger.new(passenger_params)
+
+    if @passenger.save
+      redirect_to passenger_path(@passenger.id)
+    end
+  end
+
+  def edit
+    @passenger = Passenger.find_by(id: params[:id])
+
+    if @passenger.nil?
+      redirect_to passengers_path
+      return
+    end
+  end
+
+  def update
+    @passenger = Passenger.find_by(id: params[:id])
+
+    if @passenger.nil?
+      redirect_to passengers_path
+      return
+    elsif @passenger.update(passenger_params)
+      redirect_to passengers_path
+      return
+    else
+      render :edit
+      return
+    end
+  end
+
+  def destroy
+    @passenger = Passenger.find_by(id: params[:id])
+
+    if @passenger.nil?
+      redirect_to passengers_path
+      return
+    end
+
+    @passenger.destroy
+
+    redirect_to passengers_path
+    return
+  end
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:id, :name, :phone_num)
   end
 
 end
