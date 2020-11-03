89e28c4cf606cbe7106cad18e82fe2ecf495c2bb
dnsrocha <drocha@gmail.com>
2020-11-03 11:10:55 -0800

    Implemented show, new and create methods for passengers; implemented private method for passenger strong params

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -3,4 +3,32 @@ class PassengersController < ApplicationController
   def index
     @passengers = Passenger.all
   end
+
+  def show
+    @passenger = Passenger.find_by(id: params[:id])
+    if @passenger.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    @passenger = Passenger.new(passenger_params)
+    if @passenger.save
+      redirect_to passenger_path(@passenger.id)
+      return
+    else
+      render :new
+      return
+    end
+  end
+
+  private
+  def passenger_params
+    return params.require(:passenger).permit(:id, :name, :phone_num)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'homepages#index'
+
+  get '/passengers', to: 'passengers#index', as: 'passengers'
+  get '/passengers/:id', to: 'passengers#show', as: 'passenger'
 end
