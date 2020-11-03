8e19f7d02eb2e306de824aa0c1a51d974f1ecf38
Tram Bui <pbui17@cmc.edu>
2020-11-03 15:28:45 -0600

    wrote 1 create passengers test

--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -44,6 +44,33 @@ describe PassengersController do
 
   describe "create" do
     # Your tests go here
+    it "can create a new passenger" do
+      passenger_hash = {
+          passenger: {
+              name: "Tram Bui",
+              phone_num: "000-111-2222"
+          }
+      }
+
+      expect {
+        post passengers_path, params: passenger_hash
+      }.must_differ "Passenger.count", 1
+
+      # new_passenger = Passenger.find_by
+      expect(Passenger.last.name).must_equal passenger_hash[:passenger][:name]
+      expect(Passenger.last.phone_num).must_equal passenger_hash[:passenger][:phone_num]
+
+
+      must_respond_with :redirect
+      must_redirect_to passengers_path
+
+    end
+
+    # to be filled in
+    xit "will not create a passenger with invalid params" do
+
+    end
+
   end
 
   describe "edit" do
