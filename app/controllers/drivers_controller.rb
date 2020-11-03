05b3d333e55a6130566d311224febd192c59a027
dnsrocha <drocha@gmail.com>
2020-11-03 11:56:41 -0800

    Implemented drivers show, new, create, edit, update and destroy methods for Drivers class // Implemented prived method for driver strong params

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -3,4 +3,70 @@ class DriversController < ApplicationController
   def index
     @drivers = Driver.all
   end
+
+  def show
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new(driver_params)
+    @driver.available = true
+
+    if @driver.save
+      redirect_to driver_path(@driver.id)
+      return
+    else
+      render :new, :bad_request
+      return
+    end
+  end
+
+  def edit
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      head :not_found
+      return
+    elsif @driver.update(driver_params)
+      redirect_to driver_path
+      return
+    else
+      render :edit
+      return
+    end
+  end
+
+  def destroy
+    @driver = Driver.find_by(id: params[:id])
+    if @driver.nil?
+      head :not_found
+      return
+    else
+      @driver.destroy
+      redirect_to drivers_path
+      return
+    end
+  end
+
+
+  private
+  def driver_params
+    return params.require(:driver).permit(:id, :name, :vin, :available)
+  end
+
 end
