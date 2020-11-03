c46174ea43f704432caf70f2528188aee224fc93
Jasmine <jasylop@gmail.com>
2020-11-03 13:25:54 -0800

    set up the controller with the 7 actions & driver_params

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,47 @@
 class DriversController < ApplicationController
+
+  def index
+    @drivers = Driver.all
+  end
+
+  def show
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.nil?
+      render :notfound, status: :not_found
+    end
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new(driver_params)
+
+    if @driver.save
+      redirect_to driver_path(@driver)
+    else # save failed?
+      render :new
+    end
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+    @driver = Driver.find_by(id: params[:id])
+  end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin, :available)
+  end
 end
