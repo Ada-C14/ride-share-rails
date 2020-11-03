fd7cdcd035e68d1c8abcfef2eb6508e70bf065fe
Mackenzie <mvlofthus@gmail.com>
2020-11-03 15:50:25 -0800

    added new and edit to views, controller

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -12,4 +12,49 @@ class PassengersController < ApplicationController
     end
   end
 
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    passenger = Passenger.new(
+        name: params[:passenger][:name],
+        phone_num: params[:passenger][:phone_num]
+        )
+
+    if passenger.save
+      redirect_to passenger_path(passenger) #send to show
+    else
+      render :new
+    end
+  end
+
+  def update
+    @passenger = Passenger.find_by(id: params[:id])
+    if @passenger.nil?
+      head :not_found
+      return
+    elsif @passenger.update(
+        name: params[:passenger][:name],
+        phone_num: params[:passenger][:phone_num]
+    )
+      redirect_to passenger_path # go to the index so we can see the book in the list
+      return
+    else # save failed :(
+    render :edit
+    return
+    end
+
+  end
+
+  def edit
+    @passenger = Passenger.find_by(id: params[:id])
+
+    if @passenger.nil?
+      redirect_to passengers_path
+      return
+    end
+
+  end
+
 end
