3edc2d7f770964d2bb780727c3ef712d6368c538
Marj E <marj@marjorieetienne.com>
2020-11-03 11:38:36 -0800

    added model association and seeded db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_023206) do
+ActiveRecord::Schema.define(version: 2020_11_03_192846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,12 +31,15 @@ ActiveRecord::Schema.define(version: 2020_11_03_023206) do
   end
 
   create_table "trips", force: :cascade do |t|
-    t.integer "driver_id"
-    t.integer "passenger_id"
     t.datetime "date"
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "driver_id"
+    t.bigint "passenger_id"
+    t.integer "cost"
+    t.index ["driver_id"], name: "index_trips_on_driver_id"
+    t.index ["passenger_id"], name: "index_trips_on_passenger_id"
   end
 
 end
