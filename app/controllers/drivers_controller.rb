c531d006046c5aa8ef8661a241726f4a9c440edf
Rachael Gomez <gomezrc1220@gmail.com>
2020-11-02 22:15:00 -0800

    relationships not working right but data seeded and index and show page for driver is working.

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 class DriversController < ApplicationController
-  has_many :trips
+  # has_many :trips
   def index
     @drivers = Driver.all
   end
 
   def show
     driver_id = params[:id]
-    @driver = Task.find_by(id: driver_id)
+    @driver = Driver.find_by(id: driver_id)
     if @driver.nil?
       redirect_to drivers_path
       return
