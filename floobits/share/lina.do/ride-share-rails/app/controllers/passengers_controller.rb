20f4aa50bf1f520431c7a9746e2cc97b0ed5f0dd
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-02 17:58:22 -0800

    Created _form partial, new and edit views, new/create and edit/update controller actions

--- floobits/share/lina.do/ride-share-rails/app/controllers/passengers_controller.rb
+++ floobits/share/lina.do/ride-share-rails/app/controllers/passengers_controller.rb
@@ -12,6 +12,7 @@ class PassengersController < ApplicationController
       redirect_to passengers_path
       return
     end
+
   end
 
   def new
@@ -19,19 +20,52 @@ class PassengersController < ApplicationController
   end
 
   def create
+    @passenger = Passenger.new(passenger_params)
+
+    if @passenger.save
+      redirect_to passenger_path(@passenger.id)
+      return
+    else
+      redirect_to passengers_path
+      return
+    end
 
   end
 
   def edit
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
 
+    if @passenger.nil?
+      redirect_to passengers_path
+      return
+    end
   end
 
   def update
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
 
+    if @passenger.nil?
+      redirect_to passengers_path
+      return
+    elsif @passenger.update(passenger_params)
+      redirect_to passenger_path(@passenger.id)
+      return
+    else
+      render :edit
+      return
+    end
   end
 
   def destroy
 
+  end
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_number)
   end
 
 end
