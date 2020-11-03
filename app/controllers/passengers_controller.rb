af3e16c0cf52c8d3323f883755dab115a94ae102
Rachael Gomez <gomezrc1220@gmail.com>
2020-11-03 14:19:49 -0800

    moved has_many from controller to model for all files that needed it

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,5 +1,4 @@
 class PassengersController < ApplicationController
-  has_many :trips
   def index
     @passengers = Passenger.all
   end
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,6 +1,4 @@
 class TripsController < ApplicationController
-  belongs_to :driver
-  belongs_to :passenger
   def index
     @trips = Trip.all
   end
