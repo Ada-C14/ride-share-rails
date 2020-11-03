38ac031daefbc9628dbc641667391f3d1e9fd31e
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-02 19:46:11 -0800

    Removed Christabel's weird nested save structure

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -6,7 +6,35 @@ class PassengersController < ApplicationController
 
   def show
     passenger_id = params[:id].to_i
-    @passenger = Task.find_by(id: passenger_id)
+    @passenger = Passenger.find_by(id: passenger_id)
+
+    if @passenger.nil?
+      redirect_to passengers_path
+      return
+    end
+
+  end
+
+  def new
+    @passenger = Passenger.new
+  end
+
+  def create
+    @passenger = Passenger.new(passenger_params)
+
+    if @passenger.save
+      redirect_to passenger_path(@passenger.id)
+      return
+    else
+      redirect_to passengers_path
+      return
+    end
+
+  end
+
+  def edit
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
 
     if @passenger.nil?
       redirect_to passengers_path
@@ -14,24 +42,30 @@ class PassengersController < ApplicationController
     end
   end
 
-  def new
-
-  end
-
-  def create
-
-  end
-
-  def edit
-
-  end
-
   def update
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
 
+    if @passenger.nil?
+      redirect_to passengers_path
+      return
+    elsif @passenger.update(passenger_params)
+      redirect_to passenger_path(@passenger.id)
+      return
+    else
+      render :edit
+      return
+    end
   end
 
   def destroy
 
+  end
+
+  private
+
+  def passenger_params
+    return params.require(:passenger).permit(:name, :phone_number)
   end
 
 end
