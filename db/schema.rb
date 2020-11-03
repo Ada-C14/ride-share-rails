4df90de218ad13bd2d35a42d4ddbef62caf3c56e
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-03 15:47:10 -0800

    relate trips to passengers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_233817) do
+ActiveRecord::Schema.define(version: 2020_11_03_234426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,9 @@ ActiveRecord::Schema.define(version: 2020_11_03_233817) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "driver_id"
+    t.bigint "passenger_id"
     t.index ["driver_id"], name: "index_trips_on_driver_id"
+    t.index ["passenger_id"], name: "index_trips_on_passenger_id"
   end
 
 end
