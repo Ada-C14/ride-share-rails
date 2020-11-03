7cf66e413f9d9a305f960941f7e7c2f7b71850a0
Leah Scott-Zechlin <scottzec@gmail.com>
2020-11-03 13:07:07 -0800

    created routes and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #
+
+  root to: 'rideshare#index'
+
+  resources :rideshare, only: [:index]
+
+  scope '/rideshare' do
+    resources :passengers # only[:show, :index]
+    resources :drivers
+    resources :trips
+  end
 end
