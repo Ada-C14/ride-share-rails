e1d2477db0bf7662867e5216637a8ce4180b1eca
Lina Do <l.do5147@gmail.com>
2020-11-02 16:55:36 -0800

    created model for Passenger and filled out index view

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,11 +1,17 @@
 class PassengersController < ApplicationController
 
   def index
-
+    @passengers = Passenger.all.order(:id)
   end
 
   def show
+    passenger_id = params[:id].to_i
+    @passenger = Task.find_by(id: passenger_id)
 
+    if @passenger.nil?
+      redirect_to passengers_path
+      return
+    end
   end
 
   def new
