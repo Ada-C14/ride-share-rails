899ac3133227c99d9dc4c644c3aaa4ca42c19a65
MonaRahmani <monarahmanimehr@gmail.com>
2020-11-02 21:01:15 -0800

    controllers action added

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,27 @@
 class TripsController < ApplicationController
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def trip_params
+  end
+
+
 end
