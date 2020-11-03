861b527b9f9f83ca98809bdecb62aab1a4e27def
Lina Do <l.do5147@gmail.com>
2020-11-03 14:30:38 -0800

    Created drivers controller, route, and Restful actions

---
+++ app/controllers/drivers_controller.rb
@@ -0,0 +1,84 @@
+class DriversController < ApplicationController
+
+  def index
+    @drivers = Driver.all.order(:id)
+  end
+
+  def show
+    driver_id = params[:id].to_i
+    @driver = Driver.find_by(id: driver_id)
+
+    if @driver.nil?
+      redirect_to drivers_path
+      return
+    end
+
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new(driver_params)
+
+    if @driver.save
+      redirect_to driver_path(@driver.id)
+      return
+    else
+      redirect_to drivers_path
+      return
+    end
+
+  end
+
+  def edit
+    driver_id = params[:id]
+    @driver = Driver.find_by(id: driver_id)
+
+    if @driver.nil?
+      redirect_to drivers_path
+      return
+    end
+  end
+
+  def update
+    driver_id = params[:id]
+    @driver = Driver.find_by(id: driver_id)
+
+    if @driver.nil?
+      redirect_to drivers_path
+      return
+    elsif @driver.update(driver_params)
+      redirect_to driver_path(@driver.id)
+      return
+    else
+      render :edit
+      return
+    end
+  end
+
+  def destroy
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.nil?
+      head :not_found
+      return
+    end
+
+    if @driver.destroy
+      redirect_to drivers_path
+      return
+    else #if .destroy fails
+    redirect_to driver_path(@driver.id)
+    return
+    end
+  end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin, :is_available)
+  end
+
+end
