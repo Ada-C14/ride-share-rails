8b53161ee9f16066c622acbcac146c74b249e73e
Tram Bui <pbui17@cmc.edu>
2020-11-03 10:24:38 -0600

    wrote tests for passenger show and new

--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -16,11 +16,30 @@ describe PassengersController do
   end
 
   describe "show" do
-    # Your tests go here
+    it "can get to the passenger details page" do
+
+      get passenger_path(passenger.id)
+
+      must_respond_with :success
+
+    end
+
+    it "will show not_found if the passenger id is not found" do
+      get passenger_path(-1)
+
+      # or should it redirect?
+      must_respond_with :not_found
+    end
   end
 
   describe "new" do
     # Your tests go here
+    it "can get to the new passenger page" do
+
+      get new_passenger_path
+
+      must_respond_with :success
+    end
   end
 
   describe "create" do
