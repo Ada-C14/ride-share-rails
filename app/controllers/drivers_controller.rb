114785d11ca8001faeab6d9ca68555f2f9d1ea32
Rachael Gomez <gomezrc1220@gmail.com>
2020-11-02 20:22:41 -0800

    made driver, trip, and passenger models and migrated them

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,18 @@
+# frozen_string_literal: true
+
 class DriversController < ApplicationController
+  def index
+    @drivers = Driver.all
+  end
+
+  def show
+    driver_id = params[:id]
+    @task = Task.find_by(id: task_id)
+    if @task.nil?
+      redirect_to tasks_path
+      return
+      head :not_found
+      return
+    end
+  end
 end
