a620372d6497f9c551de7d79b3acceb6ee5a2d90
roshni-patel <rpatel01@wesleyan.edu>
2020-11-02 18:20:44 -0600

    Set up the models, controllers, and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :drivers
+  resources :passengers
+  resources :trips
+
+  # get 'trips/index'
+  # get 'passengers/index'
+  # get 'drivers/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
