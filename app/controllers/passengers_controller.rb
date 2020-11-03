a72e1454ed48faf08c6d82afe35e12c3218abf6c
Tram Bui <pbui17@cmc.edu>
2020-11-03 09:59:21 -0600

    wrote index and show actions in controllers and made views for them

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,2 +1,22 @@
 class PassengersController < ApplicationController
+  def index
+    @passengers = Passenger.all
+  end
+
+  def show
+    @passenger = Passenger.find_by(id: params[:id])
+
+    if @passenger.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def new
+    @passenger = Passenger.new
+  end
+
+
+
+
 end
