b527fecc2b8705380208d32098a15c5991880c69
Mackenzie <mvlofthus@gmail.com>
2020-11-03 15:25:28 -0800

    index and show methods and views for passenger and driver

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,16 @@
 class DriversController < ApplicationController
+  def index
+    @drivers = Driver.all
+  end
+
+  def show
+    driver_id = params[:id].to_i
+    @driver = Driver.find_by(id: driver_id)
+    if @driver.nil?
+      redirect_to drivers_path
+      return
+    end
+  end
+
+
 end
--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,2 +1,15 @@
 class PassengersController < ApplicationController
+  def index
+    @passengers = Passenger.all
+  end
+
+  def show
+    passenger_id = params[:id].to_i
+    @passenger = Passenger.find_by(id: passenger_id)
+    if @passenger.nil?
+      redirect_to passengers_path
+      return
+    end
+  end
+
 end
