48875b14a74d3c74bf25aec0c096ab43aa262d87
Stacy <lundquist.stacy@gmail.com>
2020-11-03 21:27:14 -0800

    uhhhh, something happened

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
 
-  root to: 'rideshare#index'
+  root to: 'drivers#index'
 
   resources :rideshare, only: [:index]
 
