ba9e93ec1b75c8751acb8ab0ea84248bb68d56ea
Taylor Tompkins <taylormililani@gmail.com>
2020-11-03 17:28:14 -0800

    trips and passengers controllers

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,68 @@
 class TripsController < ApplicationController
+  def index
+    @trips = Trip.all
+  end
+
+  def show
+    @trip = Trip.find_by(id: params[:id])
+    if @trip.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+    if @trip.save
+      redirect_to trips_path
+      return
+    else
+      render :new
+      return
+    end
+  end
+
+  def edit
+    @trip = Trip.find_by(id: params[:id])
+    if @trip.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+    @trip = Trip.find_by(id: params[:id])
+    if @trip.nil?
+      head :not_found
+      return
+    elsif @trip.update(driver_params)
+      redirect_to trip_path(id: @trip[:id])
+      return
+    else
+      render :edit
+      return
+    end
+  end
+
+  def destroy
+    @trip = Trip.find_by(id: params[:id])
+    if @trip.nil?
+      head :not_found
+      return
+    else
+      @trip.destroy
+      redirect_to root_path
+      return
+    end
+  end
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(:date, :rating, :cost)
+  end
 end
