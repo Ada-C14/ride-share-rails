952ee14930dcccca59220f253aa3a746a2980edd
Li Dai <li.lea.dai@gmail.com>
2020-11-03 19:37:11 -0800

    added basic actions of controllers, routes and show pages for drivers

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,72 @@
 class DriversController < ApplicationController
+  def index
+    @drivers = Driver.all
+  end
+
+  def show
+    driver_id = params[:id].to_i
+    @driver = Driver.find_by(id: driver_id)
+
+    if @driver.nil?
+      redirect_to drivers_path
+      return
+    end
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new(driver_params)
+
+    if @driver.save
+      redirect_to driver_path(@driver.id)
+    end
+  end
+
+  def edit
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.nil?
+      redirect_to drivers_path
+      return
+    end
+  end
+
+  def update
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.nil?
+      redirect_to drivers_path
+      return
+    elsif @driver.update(driver_params)
+      redirect_to drivers_path
+      return
+    else
+      render :edit
+      return
+    end
+  end
+
+  def destroy
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.nil?
+      redirect_to drivers_path
+      return
+    end
+
+    @driver.destroy
+
+    redirect_to drivers_path
+    return
+  end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:id, :name, :vin, :available)
+  end
+
 end
