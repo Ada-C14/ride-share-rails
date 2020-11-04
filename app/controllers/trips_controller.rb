613d8d30e53979602880a8874215339ae806c105
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-03 22:01:20 -0800

    def CRUD methods for trips

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,65 @@
 class TripsController < ApplicationController
+  def index
+    @trips = Trip.all
+  end
+
+  def show
+    @trip = Trip.find_by(id: params[:id])
+
+    if @trip.nil?
+      redirect_to trips_path and return
+    end
+  end
+
+  def new
+    @trip = Trip.new
+  end
+
+  def create
+    @trip = Trip.new(trip_params)
+
+    if @trip.save
+      redirect_to trip_path(@trip) and return
+    else
+      render :new and return
+    end
+  end
+
+  def edit
+    @trip = Trip.find_by(id: params[:id])
+
+    if @trip.nil?
+      redirect_to trips_path and return
+    end
+  end
+
+  def update
+    @trip = Trip.find_by(id: params[:id])
+
+    if @trip.nil?
+      redirect_to trips_path and return
+    elsif
+      @trip.update(trip_params)
+      redirect_to trip_path(@trip)
+    else
+      render :edit and return
+    end
+  end
+
+  def destroy
+    @trip = Trip.find_by(id: params[:id])
+
+    if @trip
+      @trip.destroy
+      redirect_to trips_path and return
+    else
+      redirect_to trips_path and return
+    end
+  end
+
+  private
+
+  def trip_params
+    return params.require(:trip).permit(:date, :rating, :cost, :driver_id, :passenger_id)
+  end
 end
