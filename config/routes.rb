8ddd116c8790bef52001fc2525f13fefb52e21ac
Sophie Messing <sophie.e.messing@gmail.com>
2020-11-03 18:13:31 -0800

    edited routes for trips

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root to: 'homepages#index'
-  resources :drivers, :passengers, :trips
+  resources :drivers, :passengers
+  resources :trips, except[:new, :index]
   resources :homepages, only:[:index]
 end
