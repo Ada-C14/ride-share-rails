b950c5b13a8f5d3264f8e5daaafda9c88cef9062
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-03 20:07:03 -0800

    define CRUD actions in DriversController

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,4 +1,64 @@
 class DriversController < ApplicationController
+  def index
+    @drivers = Driver.all
+  end
 
-  
+  def show
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.nil?
+      redirect_to drivers_path and return
+    end
+  end
+
+  def new
+    @driver = Driver.new
+  end
+
+  def create
+    @driver = Driver.new(driver_params)
+
+    if @driver.save
+      redirect_to driver_path(@driver) and return
+    else
+      render :new and return
+    end
+  end
+
+  def edit
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver.nil?
+      redirect_to drivers_path and return
+    end
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+
+    if @driver.nil?
+      redirect_to drivers_path and return
+    elsif @driver.update(driver_params)
+      redirect_to driver_path(@driver) and return
+    else
+      render :edit and return
+    end
+  end
+
+  def destroy
+    @driver = Driver.find_by(id: params[:id])
+
+    if @driver
+      @driver.destroy
+      redirect_to drivers_path and return
+    else
+      redirect_to drivers_path and return
+    end
+  end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin, :available)
+  end
 end
