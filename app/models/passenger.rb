f0dad0b1a419c49dc7031e9c906988ba03069d1d
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-03 22:38:59 -0800

    update method name

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,7 +1,7 @@
 class Passenger < ApplicationRecord
   has_many :trips
 
-  def total_cost
+  def total_spending
     return 0 if self.trips.empty?
 
     return (self.trips.sum { |trip| trip.cost.to_f }) / 100
