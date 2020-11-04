96ac4b85abbc337dabc174a72a41b91e707731ea
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-03 22:43:40 -0800

    add avg_rating method in driver model

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,7 +3,9 @@ class Driver < ApplicationRecord
 
   # business logic
   def avg_rating
+    return 0 if self.trips.empty?
 
+    return (self.trips.sum { |trip| trip.rating.to_f } / self.trips.length).round(2)
   end
 
   def total_earnings
