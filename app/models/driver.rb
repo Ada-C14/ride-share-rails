8751e5115a2764993d35aa4fcd1735893a265f34
Ringo Alcock <ringo.alcock@gmail.com>
2020-11-04 10:26:52 -0800

    add validations to trip and driver class

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,8 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+  validates :name, presence: true
+  validates :vin, presence: true, length: { is: 17 }
+  validates :available, presence: true, inclusion: { in: [true, false] }
+
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,10 @@
 class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
+
+  validates :driver, presence: true
+  validates :passenger, presence: true
+  validates :date, presence: true
+  validates :cost, presence: true, numericality: { only_integer: true }
+  validates :rating, numericality: { only_integer: true, greater_than: 0, less_than: 6 }
 end
