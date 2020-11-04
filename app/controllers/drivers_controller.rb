7d84beb7d5b09e307da3e89b79135d0a4ffebb5d
Leah Scott-Zechlin <scottzec@gmail.com>
2020-11-04 14:45:09 -0800

    show method

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -1,2 +1,14 @@
 class DriversController < ApplicationController
+  def index
+    @drivers = Driver.all
+  end
+
+  def show
+    driver_id = params[:id] #we'll be able to access our route parameter via a special hash provided by Rails called params. The ID sent by the browser will be stored under the key :id (remember that this is the name we gave the parameter in the routefile).
+    @driver = Driver.find_by(id: driver_id)
+    if @task.nil?
+      redirect_to drivers_path
+      return
+    end
+  end
 end
