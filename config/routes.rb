6f41370490e80ee1c3681931c2f3790992d0e3c3
Jasmine <jasylop@gmail.com>
2020-11-03 17:08:24 -0800

    added the homepage & updates the trips routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # what should our root_path be?
+  root to: 'homepages#index'
 
-  resources :trips
+  resources :trips, except: [:index, :new]
   resources :drivers
   resources :passengers
 
