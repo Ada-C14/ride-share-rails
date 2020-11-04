794cd11b46718b092e5d48d8c4b2dcb383d6288a
Tram Bui <pbui17@cmc.edu>
2020-11-04 00:12:22 -0600

    calculated total_cost in passenger model and displayed in show view

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,4 +3,21 @@ class Passenger < ApplicationRecord
   validates :phone_num, presence: true
 
   has_many :trips
+
+  def total_cost
+    # get the of trips for the passenger
+    # add the cost together
+    # sum = 0
+
+    if self.trips.empty?
+      return 0
+    else
+      # self.trips.each do |trip|
+      #   sum += trip.cost
+      # end
+      return self.trips.map{|trip| trip.cost}.sum
+    end
+    # return sum
+  end
+
 end
