6f19d9dce331e8c43b1ca7cce4cd63e3ab9300da
Rachael Gomez <gomezrc1220@gmail.com>
2020-11-04 15:57:03 -0800

    added cost though not finished. shows trips but not formatted into a nice table yet.

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,9 +3,6 @@ class Driver < ApplicationRecord
   has_many :trips
   def average_rating(driver)
     ratings = driver.trips.map {|trip| trip.rating}
-    trips.each do |trip|
-      ratings << trip[:rating]
-    end
     total_ratings = 0
       ratings.each do |rating|
         total_ratings += rating
@@ -13,4 +10,36 @@ class Driver < ApplicationRecord
     average = total_ratings.to_f / ratings.length
     return average.round(1)
   end
+  def total_money(driver)
+    costs = driver.trips.map {|trip| trip.cost}
+    total = 0
+    costs.each do |cost|
+      total += cost
+    end
+    return total
+
+  end
+
+  def list_all_trips(driver)
+    # trip_ids = driver.trips.map {|trip| trip.id}
+    # trip_passengers = driver.trips.map {|trip| trip.passenger_id}
+    # trip_dates = driver.trips.map {|trip| trip.date}
+    # trip_rating = driver.trips.map {|trip| trip.rating}
+    # trip_cost = driver.trips.map {|trip| trip.cost}
+    trips = driver.trips.map {|trip| trip}
+    trips_hash = Hash[trips.map {|key, value| [key, value]}]
+    # trips_hash = trips_hash.delete(:created_at)
+    return trips_hash
+  end
+
+  # def better_hash(driver)
+  #   keys = [:created_at, :updated_at]
+  #   trips_hash = list_all_trips(driver)
+  #   new_hash = trips_hash.delete(:created_at)
+  #   # new_hash = trips_hash.delete(:updated_at)
+  #   return new_hash
+
+
+
+  # end
 end
