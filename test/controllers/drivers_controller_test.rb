3c1370d1c7b6d7173a6b0cf8968e4bb5ca648417
Genevieve Neely <genevieve.hood@gmail.com>
2020-11-03 16:41:03 -0800

    driver_controller index, view + tests

--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -2,25 +2,25 @@ require "test_helper"
 
 describe DriversController do
   # Note: If any of these tests have names that conflict with either the requirements or your team's decisions, feel empowered to change the test names. For example, if a given test name says "responds with 404" but your team's decision is to respond with redirect, please change the test name.
-
+  let (:driver) {
+    Driver.create(name: "Driver 1", vin: "1234")
+  }
   describe "index" do
     it "responds with success when there are many drivers saved" do
-      # Arrange
-      # Ensure that there is at least one Driver saved
+      driver
+      Driver.create(name: "Driver2", vin: "2345")
 
-      # Act
+      get drivers_path
 
-      # Assert
+      must_respond_with :success
 
     end
 
     it "responds with success when there are no drivers saved" do
-      # Arrange
-      # Ensure that there are zero drivers saved
 
-      # Act
+      get drivers_path
 
-      # Assert
+      must_respond_with :success
 
     end
   end
@@ -79,7 +79,7 @@ describe DriversController do
 
     end
   end
-  
+
   describe "edit" do
     it "responds with success when getting the edit page for an existing, valid driver" do
       # Arrange
