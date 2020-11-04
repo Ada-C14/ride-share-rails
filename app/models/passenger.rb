4da6e789844992a381e7fbeadb5fd91c8be15dff
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-11-03 22:37:14 -0800

    add total cost method in passenger model

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,9 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  def total_cost
+    return 0 if self.trips.empty?
+
+    return (self.trips.sum { |trip| trip.cost.to_f }) / 100
+  end
 end
