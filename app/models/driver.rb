ac9a1bede24056614e89cab997d8ccaf82050de5
India Kato <india.kato@gmail.com>
2020-11-04 15:05:52 -0800

    added driver validations

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,6 +1,9 @@
 class Driver < ApplicationRecord
     has_many :trips
 
+    validates :name, presence: true
+    validates :vin, presence: true
+
     def average_rating
         return self.trips.average(:rating)
     end
