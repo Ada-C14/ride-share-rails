35ccc23a9eb58801dd384cf4bc833994210e2911
Ada <ada@Adas-MacBook-Pro.local>
2020-11-04 18:14:22 -0500

    Passenger Model Test

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -5,11 +5,9 @@ class Passenger < ApplicationRecord
   validates :phone_num, presence: true
 
   def total_amount_charged
-    # books_with_year = self.books.where.not(publication_date: nil
+    return 0 if self.trips.empty?
     trips_with_cost = self.trips.where.not(cost: nil)
-    # years_published = author.books.map { |book| book.publication_date }
     total_amount_charged = trips_with_cost.map{ |trip| trip.cost}
-    # return first_book.publication_date
     return total_amount_charged.sum
   end
 
