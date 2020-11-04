b445655e95e8df4de708e12d05bce2dcccc83289
MonaRahmani <monarahmanimehr@gmail.com>
2020-11-03 19:33:17 -0800

    edit and delete created for trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -16,12 +16,26 @@ class TripsController < ApplicationController
   end
 
   def edit
+    @trip = Trip.find_by(id: params[:id])
+    if @trip.nil?
+      redirect_to root_path
+      return
+    end
   end
 
   def update
   end
 
   def destroy
+    trip = Trip.find_by(id: params[:id])
+
+    if trip.nil?
+      head :not_found
+      return
+    else
+      trip.destroy
+      redirect_to trips_path
+    end
   end
 
   private
