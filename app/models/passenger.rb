96703d9f6514ac56646178426d18bc1e270d394d
r-spiel <richelle.mehlhaff@gmail.com>
2020-11-04 18:34:49 -0800

    add custom model method for passenger and test/ view

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,18 @@ class Passenger < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :phone_num, presence: true
+
+  def total_charged
+    trips = self.trips.where.not(cost: nil)
+    return 0 if trips.empty?
+
+    trips_costs = trips.map { |trip| trip.cost}
+    total = (trips_costs.sum.to_f)/100
+
+    return total
+  end
+
+  def request_ride
+
+  end
 end
