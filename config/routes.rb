462c311113125b6ce2cc792b8d8f3bd5fc83eade
Iris Lux <iris.lux0@gmail.com>
2020-11-04 22:02:26 -0800

    nested routes for passenger/trips

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :drivers, :passengers, :trips
 
+  resources :passengers do
+    resources :trips, only: [:index, :new]
+  end
   #root to: do the root
 end
