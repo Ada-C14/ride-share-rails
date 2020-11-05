0a1df265fcf0d291194bdada18795120472a1f20
Kareha Agesa <kmaagesa@gmail.com>
2020-11-04 22:18:34 -0800

    debug total_revenue

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -9,7 +9,8 @@ class Driver < ApplicationRecord
     earning_rate = 0.8
 
     self.trips.each do |trip|
-      total += trip.cost - fee_per_trip unless trip.end_time.nil?
+      # unless trip.end_time.nil???????
+      total += trip.cost - fee_per_trip
     end
 
     earning = total * earning_rate
