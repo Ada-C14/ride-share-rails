8d239aadf986aa03c5afc3c7983a6a73d90a8b80
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-11-04 18:11:57 -0800

    Fixed syntax error for driver vin validation, added nav and footer to homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'homepages#index'
+  root to: 'homepages#index', as: 'homepage'
 
     resources :trips, only: [:show, :edit, :update, :destroy]
 
