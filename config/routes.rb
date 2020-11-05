c622a3399393a74ef0a358e94c68c70b7d244f05
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-11-04 17:40:10 -0800

    generate hompages controller and set it as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root 'drivers#index'
+  root 'homepages#index'
   resources :drivers do
     resources :trips, only: [:index]
   end
