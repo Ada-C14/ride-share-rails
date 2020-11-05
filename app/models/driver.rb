fd1ac3778ca056e72c42d9f9ad706f94fb298748
Anna Kim <anna.kim93@gmail.com>
2020-11-04 23:11:10 -0500

    completed working versions of average rating and total earnings for driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,10 +3,19 @@ class Driver < ApplicationRecord
   validates :name, :vin, presence: true
 
   def total_earnings
+    total_earnings = self.trips.sum(:cost)
 
+    return total_earnings
   end
 
   def average_rating
+    avg_rating = self.trips.where.not(rating: nil).sum(:rating)
+    total_trips = self.trips.count(:rating)
 
+    return nil if total_trips.zero? || avg_rating.zero?
+
+    avg_rating /= total_trips
+
+    return avg_rating
   end
 end
