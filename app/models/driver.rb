30e3cb00108bb2244607dc8c09b399f00ee49e19
Anna Kim <anna.kim93@gmail.com>
2020-11-04 23:15:14 -0500

    fixed calculating for driver lifetime earnings

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,9 +3,12 @@ class Driver < ApplicationRecord
   validates :name, :vin, presence: true
 
   def total_earnings
+    per_trip_fee = 1.65
     total_earnings = self.trips.sum(:cost)
+    total_earnings_net_fees = total_earnings - per_trip_fee * self.trips.count
+    net_earnings = total_earnings_net_fees * 0.8
 
-    return total_earnings
+    return net_earnings
   end
 
   def average_rating
