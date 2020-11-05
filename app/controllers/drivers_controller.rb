026555fcac3aa5c8f8ea22266175917e39e1865a
Blaine <b.atsbeha@gmail.com>
2020-11-05 15:36:06 -0800

    Working on edit function

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -24,49 +24,36 @@ class DriversController < ApplicationController
     # @driver = Driver.new(driver_params_results
     @drivers = Driver.new(name: params[:driver][:name], vin: params[:driver][:vin])
     if @drivers.save
-      redirect_to drivers_path(@driver.id)
+      redirect_to drivers_path(@drivers.id)
       return
     else
       render :new
       return
     end
+  end
 
   def edit
     @drivers = Driver.find_by(id:params[:id])
 
     if @drivers.nil?
-      redirect_to driver_path(@drivers.id) #Vs. Head :not_found
+      redirect_to drivers_path(@drivers.id) #Vs. Head :not_found
       return
     end
   end
 
   def update
-    @driver = Driver.find_by(id: params[:id])
+    @drivers = Driver.find_by(id: params[:id])
 
-    if @driver.nil?
+    if @drivers.nil?
       head :not_found #redirect_to driver_path(@driver.id) # vs Head :not_found
       return
-    elsif @driver.update(driver_params)
-      redirect_to new_driver_path(@driver.id)
+    elsif @drivers.update(driver_params)
+      redirect_to driver_path(@drivers.id)
       return
     else
       render :edit
       return
       end
-  end
-
-  def create
-    driver_params_results = driver_params
-    driver_params_results[:available] = true
-
-    @drivers = Driver.new(driver_params_results)
-    if @drivers.save
-      redirect_to driver_path(@driver.id)
-      return
-    else
-      render :new
-      return
-    end
   end
 
   def destroy
@@ -81,6 +68,6 @@ class DriversController < ApplicationController
   def driver_params
     return params.require(:driver).permit(:name, :vin, :available)
   end
-
 end
+
 
