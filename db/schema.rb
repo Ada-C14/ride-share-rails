edf8894f60729354d0b0089369699f6b72e02135
Genevieve Neely <genevieve.hood@gmail.com>
2020-11-05 14:30:40 -0800

    change cost to integer + update view formatting

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_04_002449) do
+ActiveRecord::Schema.define(version: 2020_11_05_221800) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2020_11_04_002449) do
 
   create_table "trips", force: :cascade do |t|
     t.integer "rating"
-    t.float "cost"
+    t.integer "cost"
     t.string "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
