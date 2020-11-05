e7d0f1ad3b10ce25c23e7c7d8dd451194afc0360
Anna Kim <anna.kim93@gmail.com>
2020-11-04 19:01:23 -0500

    nested routes to allow for trips info associated with driver and passenger

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,11 @@
 class TripsController < ApplicationController
 
+  def something
+
+  end
+
+  private
+
   def trip_params
     return params.require(:trip).permit(:driver_id, :passenger_id, :date, :rating, :cost)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   root to: 'homepages#index'
 
-  resources :drivers
-  resources :passengers
-  resources :trips
+  resources :drivers do
+    resources :trips, only: [:index]
+  end
+
+  resources :passengers do
+    resources :trips, only: [:index, :create]
+  end
+
+  resources :trips, except: [:new]
 end
