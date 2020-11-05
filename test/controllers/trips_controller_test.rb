7ed073ab5432c2de3b013a019f6c4ce6adc52d86
Iris Lux <iris.lux0@gmail.com>
2020-11-05 01:56:51 -0800

    tests for create and show

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -1,12 +1,48 @@
 require "test_helper"
 
 describe TripsController do
+
+
+  let(:trip){
+    Trip.create(date: Date.current)
+  }
+
+  let(:passenger){
+    Passenger.create name: "sample passenger", phone_num: "999-999-9999"
+  }
+
   describe "show" do
-    # Your tests go here
+
+    it "can get a valid trip" do
+
+      # Act
+      get trip_path(trip.id)
+
+      # Assert
+      must_respond_with :success
+    end
+
+    it "will redirect for an invalid trip" do
+
+      # Act
+      get trip_path(-1)
+
+      # Assert
+      must_respond_with :redirect
+    end
   end
 
   describe "create" do
     # Your tests go here
+    it "can create a new trip" do
+
+      Driver.create name: "Bernardo Prosacco", vin: "WBWSS52P9NEYLVDE9", available: true
+
+      expect{
+        post passenger_trips_path(passenger.id)
+      }.must_change "Trip.count", 1
+
+    end
   end
 
   describe "edit" do
