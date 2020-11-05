c105d39b0156d7a297a7bd70abb9e20561c54ae8
Taylor Tompkins <taylormililani@gmail.com>
2020-11-05 15:36:39 -0800

    still working on trip controller tests

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -1,13 +1,31 @@
 require "test_helper"
 
 describe TripsController do
+  before do
+    Driver.create(name: "TEST123", vin: "WBWSS52P9NEYLVDE9", available: true)
+    @passenger = Passenger.create(name: "Judy", phone_num: "360-555-0987")
+    @trip = Trip.create(driver_id: Driver.first.id, passenger_id: Passenger.first.id, date: "2016-04-05", rating: 3, cost: 1293)
+  end
   describe "show" do
     it "can get a valid trip" do
-      # Your tests go here
+
+      valid_trip_id = @trip.id
+
+      # Act
+      get "/trips/#{ valid_trip_id }"
+
+      # Assert
+      must_respond_with :success
     end
 
     it "will redirect for an invalid trip" do
-      # Your tests go here
+      invalid_passenger_id = -1
+
+      # Act
+      get "/passengers/#{ invalid_passenger_id }"
+
+      # Assert
+      must_respond_with :not_found
     end
   end
 
@@ -15,15 +33,35 @@ describe TripsController do
     # test if driver status changes
     # test if found driver is available
     # test if right passenger is selected
+
+    let (:invalid_trip) {
+      {
+          trip: {
+              date: "2016-04-05",
+              rating: 50,
+              cost: 12.0
+          }
+      }
+    }
+    it "does not create a trip if the form data violates Trip validations, and responds with a redirect" do
+      # Note: This will not pass until ActiveRecord Validations lesson
+      # Arrange
+
+      # Act-Assert
+      expect {
+        post passenger_trips_path(@passenger.id), params: invalid_trip
+      }.wont_change 'Trip.count'
+
+      # Assert
+      # Check that the controller redirects
+      must_redirect_to passenger_path(@trip.id)
+
+    end
   end
 
   describe "edit" do
-    before do
-      @trip = Trip.create(driver_id: 1, passenger_id: 2, date: "2016-04-05", rating: 3, cost: 1293)
-    end
-
     it "responds with success when getting the edit page for an existing, valid trip" do
-      # Arrange & Actgit add
+      # Arrange & Act
       get edit_trip_path(@trip.id)
 
       # Assert
@@ -41,18 +79,12 @@ describe TripsController do
   end
 
   describe "update" do
-    before do
-      @trip = Trip.create(driver_id: 1, passenger_id: 2, date: "2016-04-05", rating: 3, cost: 1293)
-    end
-
     let (:valid_trip) { 
       { 
-        driver: {
-          driver_id: 1, 
-          passenger_id: 2, 
+          trip: {
           date: "2016-04-05", 
           rating: 5, 
-          cost: 12
+          cost: 12.0
         }
       }    
     }
@@ -67,8 +99,8 @@ describe TripsController do
 
       # Assert
       update_trip = Trip.find_by(id: id)
-      expect(update_trip.driver_id).must_equal valid_trip[:trip][:driver_id]
-      expect(update_trip.passenger_id).must_equal valid_trip[:trip][:passenger_id]
+      expect(update_trip.driver_id).must_equal @trip.driver_id
+      expect(update_trip.passenger_id).must_equal @trip.passenger_id
       expect(update_trip.date).must_equal valid_trip[:trip][:date]
       expect(update_trip.rating).must_equal valid_trip[:trip][:rating]
       expect(update_trip.cost).must_equal valid_trip[:trip][:cost]
@@ -83,28 +115,12 @@ describe TripsController do
 
       # Act-Assert
       expect {
-        patch trip_path(id), params: trip_driver
+        patch trip_path(id), params: valid_trip
       }.wont_change 'Trip.count'
 
       # Assert
       must_respond_with :not_found
     end
-
-    it "does not create a trip if the form data violates Driver/Passenger validations, and responds with a redirect" do
-      # Note: This will not pass until ActiveRecord Validations lesson
-      # Arrange
-      # Ensure there is an existing driver saved
-      # Assign the existing driver's id to a local variable
-      # Set up the form data so that it violates Driver validations
-
-      # Act-Assert
-      # Ensure that there is no change in Driver.count
-
-      # Assert
-      # Check that the controller redirects
-
-    end
-
   end
 
   describe "destroy" do
