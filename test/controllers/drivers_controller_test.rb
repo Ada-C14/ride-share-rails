058e58a1d6f1190370935ba35f29804174738793
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-11-04 16:30:08 -0800

    Added tests for driver and passenger controllers and added an edit.html for passengers

--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -5,13 +5,11 @@ describe DriversController do
 
   describe "index" do
     it "responds with success when there are many drivers saved" do
-      # Arrange
-      # Ensure that there is at least one Driver saved
-
+      Driver.create(name: "CheezitMan", vin: "ABCDEFGHIJKLMNOPQ")
       # Act
-
+      get drivers_path
       # Assert
-
+      must_respond_with :success
     end
 
     it "responds with success when there are no drivers saved" do
@@ -19,21 +17,22 @@ describe DriversController do
       # Ensure that there are zero drivers saved
 
       # Act
-
+      get drivers_path
       # Assert
-
+      must_respond_with :success
     end
   end
 
   describe "show" do
     it "responds with success when showing an existing valid driver" do
+      driver = Driver.create(name: "CheezitMan", vin: "ABCDEFGHIJKLMNOPQ")
       # Arrange
+
       # Ensure that there is a driver saved
-
       # Act
-
+      get driver_path(driver.id)
       # Assert
-
+      must_respond_with :success
     end
 
     it "responds with 404 with an invalid driver id" do
@@ -41,14 +40,18 @@ describe DriversController do
       # Ensure that there is an id that points to no driver
 
       # Act
-
+      get driver_path(4)
       # Assert
-
+      must_respond_with :not_found
     end
   end
 
   describe "new" do
     it "responds with success" do
+      #arrange
+      get new_driver_path
+      #Assert
+      must_respond_with :success
     end
   end
 
@@ -56,17 +59,33 @@ describe DriversController do
     it "can create a new driver with valid information accurately, and redirect" do
       # Arrange
       # Set up the form data
-
+      driver_hash = {
+          driver: {
+              name: "Solomon Mehru",
+              vin: "WBWSS52P9NEYLVDE9",
+          },
+      }
       # Act-Assert
-      # Ensure that there is a change of 1 in Driver.count
+      #Ensure that there is a change of 1 in Driver.count
+      expect {
+              post drivers_path, params: driver_hash
+            }.must_change "Driver.count", 1
+
+            new_driver = Driver.find_by(name: driver_hash[:driver][:name])
+            expect(new_driver.vin).must_equal driver_hash[:driver][:vin]
+            # expect(new_driver.available).must_equal :true
+
+            must_respond_with :redirect
+            must_redirect_to driver_path(new_driver.id)
 
       # Assert
       # Find the newly created Driver, and check that all its attributes match what was given in the form data
       # Check that the controller redirected the user
+      end
 
-    end
 
     it "does not create a driver if the form data violates Driver validations, and responds with a redirect" do
+
       # Note: This will not pass until ActiveRecord Validations lesson
       # Arrange
       # Set up the form data so that it violates Driver validations
@@ -79,31 +98,48 @@ describe DriversController do
 
     end
   end
-  
   describe "edit" do
     it "responds with success when getting the edit page for an existing, valid driver" do
-      # Arrange
-      # Ensure there is an existing driver saved
+      driver = Driver.create(name: "Adie A", vin: "ABCDEFGHIJKLMNOPQ")
 
-      # Act
-
-      # Assert
+      #Arrange
+      get edit_driver_path(driver.id)
+      #Assert
+      must_respond_with :success
 
     end
 
     it "responds with redirect when getting the edit page for a non-existing driver" do
-      # Arrange
-      # Ensure there is an invalid id that points to no driver
-
       # Act
-
+      get edit_driver_path(-1)
       # Assert
-
+      must_respond_with :not_found
     end
   end
 
   describe "update" do
     it "can update an existing driver with valid information accurately, and redirect" do
+      driver = Driver.create(name: "Update Me", vin: "ABCDEFGHIJKLMNOPQ")
+
+      driver_hash = {
+          driver: {
+              name: "new name",
+              vin: "ABCDEFGHIJKLMNOPQ"
+          }
+      }
+
+      id = driver.id
+      #Assert
+      expect {
+        patch driver_path(id), params: driver_hash
+      }.must_differ 'Driver.count', 0 #updating the book, we're not adding a book
+
+      must_redirect_to driver_path(id)
+
+      new_driver = Driver.find_by(id: id)
+      expect(new_driver.name).must_equal driver_hash[:driver][:name]
+      expect(new_driver.vin).must_equal driver_hash[:driver][:vin]
+
       # Arrange
       # Ensure there is an existing driver saved
       # Assign the existing driver's id to a local variable
@@ -119,6 +155,11 @@ describe DriversController do
     end
 
     it "does not update any driver if given an invalid id, and responds with a 404" do
+      #Act
+      patch driver_path(-1)
+      #Assert
+      must_respond_with :not_found
+
       # Arrange
       # Ensure there is an invalid id that points to no driver
       # Set up the form data
@@ -132,6 +173,8 @@ describe DriversController do
     end
 
     it "does not create a driver if the form data violates Driver validations, and responds with a redirect" do
+
+
       # Note: This will not pass until ActiveRecord Validations lesson
       # Arrange
       # Ensure there is an existing driver saved
@@ -149,6 +192,19 @@ describe DriversController do
 
   describe "destroy" do
     it "destroys the driver instance in db when driver exists, then redirects" do
+      driver = Driver.create(name: "Finish Rails homework",  vin: "ABCDEFGHIJKLMNOPQ")
+      id = driver.id
+
+      #act
+      expect{
+        delete driver_path(id)
+      }.must_change "Driver.count", -1
+
+      #Assert
+      must_respond_with :redirect
+      must_redirect_to drivers_path
+
+
       # Arrange
       # Ensure there is an existing driver saved
 
@@ -161,6 +217,11 @@ describe DriversController do
     end
 
     it "does not change the db when the driver does not exist, then responds with " do
+      #Act
+      delete driver_path(-1)
+      #Assert
+      must_respond_with :not_found
+
       # Arrange
       # Ensure there is an invalid id that points to no driver
 
--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -2,30 +2,138 @@ require "test_helper"
 
 describe PassengersController do
   describe "index" do
-    # Your tests go here
+    it "responds with success when there are many passengers saved" do
+      Passenger.create(name: "CheezitMan", phone_num: "4256789098")
+      # Arrange
+      # Ensure that there is at least one Passenger saved
+      # Act
+      get passengers_path
+      # Assert
+      must_respond_with :success
+    end
+
+      it "responds with success when there are no passengers saved" do
+        get passengers_path
+
+        # Assert
+        must_respond_with :success
+      end
   end
 
   describe "show" do
-    # Your tests go here
+    it "responds with success when showing an existing valid passenger" do
+      passenger = Passenger.create(name: "CheezitMan", phone_num: "4258705678")
+      #Act
+      get passenger_path(passenger.id)
+      #Assert
+      must_respond_with :success
+
+    end
+    it "responds with 404 with an invalid passenger id" do
+      #Act
+      get passenger_path(4)
+      #Assert
+      must_respond_with :not_found
+    end
   end
 
   describe "new" do
+    it "responds with success" do
+      #Act
+      get new_passenger_path
+      #Assert
+      must_respond_with :success
     # Your tests go here
+    end
   end
 
   describe "create" do
-    # Your tests go here
-  end
+    it "can create a new passenger with valid information accurately, and redirect" do
+      passenger = Passenger.create(name: "Update Me", phone_num: "345-678-7685")
 
+      passenger_hash = {
+          passenger: {
+              name: "Solomon Mehru",
+              phone_num: "456-879-8765"
+          }
+      }
+      id = passenger.id
+      #Assert
+      expect {
+        post passengers_path(id), params: passenger_hash
+      }.must_change "Passenger.count", 1
+
+      new_passenger = Passenger.find_by(name: passenger_hash[:passenger][:name])
+      expect(new_passenger.name).must_equal passenger_hash[:passenger][:name]
+      # expect(new_passenger.name).must_equal passenger_hash[:passenger][:name]
+
+      must_respond_with :redirect
+      must_redirect_to passenger_path(new_passenger.id)
+    end
+    it "does not create a passenger if the form data violates Passenger validations, and responds with a redirect" do
+
+    end
+  end
   describe "edit" do
-    # Your tests go here
-  end
+    it "responds with success when getting the edit page for an existing, valid Passenger" do
+      passenger = Passenger.create(name: "Adie A", phone_num: "123-456-7898")
 
+      #Act
+      get edit_passenger_path(passenger.id)
+
+      #Assert
+      must_respond_with :success
+
+    end
+    it "responds with redirect when getting the edit page for a non-existing passenger" do
+      get edit_passenger_path(-1)
+      must_respond_with :not_found
+    end
+  end
   describe "update" do
-    # Your tests go here
-  end
+    it "can update an existing passenger with valid information accurately, and redirect" do
+      passenger = Passenger.create(name: "Update Me", phone_num: "123-456-7898")
+      passenger_hash = {
+        Passenger: {
+            name: "new name",
+            phone_num: "123-456-7898"
+        }
+      }
 
-  describe "destroy" do
-    # Your tests go here
+    id = passenger.id
+
+    expect {
+      patch passenger_path(id), params: passenger_hash
+    }.must_differ 'Passenger.count', 0
+
+    must_redirect_to passenger_path(id)
+
+    new_passenger = Passenger.find_by(id: id)
+    expect(new_passenger.name).must_equal passenger_hash[:passenger][:name]
+    expect(new_passenger.phone_num).must_equal passenger_hash[:passenger][:phone_num]
+    end
+    it "does not update any passenger if given an invalid id, and responds with a 404" do
+      #Act
+      patch passenger_path(-1)
+      #Assert
+      must_respond_with :not_found
+    end
+    it "does not create a passenger if the form data violates passenger validations, and responds with a redirect" do
+    end
+    end
+    describe "destroy" do
+      it "destroys the passenger instance in db when passenger exists, then redirects" do
+        passenger = Passenger.create(name: "Finish Rideshare",  phone_num: "123-987-4567")
+        id = passenger.id
+
+        #act
+        expect{
+          delete passenger_path(id)
+        }.must_change "Passenger.count", -1
+
+        #Assert
+        must_respond_with :redirect
+        must_redirect_to passengers_path
+      end
   end
 end
