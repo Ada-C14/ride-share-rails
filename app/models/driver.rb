d361544670dd4faf8099f3c9fd60a7a4b8616429
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-04 17:17:12 -0800

    wrote driver#change_status

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -5,4 +5,14 @@ class Driver < ApplicationRecord
 
   validates :vin, presence: true, uniqueness: true
 
+  def change_status
+    if self.availability_status
+      self.availability_status = false
+    else
+      self.availability_status = true
+    end
+    self.save
+    return
+  end
+
 end
