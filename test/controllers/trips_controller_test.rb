71099c1ed7079851cc56573ec943caf36c910739
dnsrocha <drocha@gmail.com>
2020-11-04 16:36:22 -0800

    Fixed tests for trips controller + added initial views for passenger

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -9,31 +9,35 @@ describe TripsController do
       must_respond_with :not_found
     end
 
-    it "can get a valid trip" do
+    it 'responds with success when showing an existing valid trip' do
+      fake_passenger = Passenger.create(name: 'CheezItMan', phone_num: '123-456-7890')
+      fake_driver = Driver.create(name: 'Devin', vin: 567, available: true)
+      trip = Trip.create(passenger_id: pass.id, driver_id: driver.id, date: Date.today, cost: 25, rating: nil)
 
-      get trip_path(trip.id)
+      get trip_path(trip)
+
       must_respond_with :success
     end
   end
 
   describe "create" do
     # Your tests go here
-    it "creates a new trip" do
-      pass = Passenger.create(name: 'Denise', phone_num: '123-456-7890')
-      driver = Driver.create(name: 'Pink Panther', vin: '123', available: true)
+    it "can create a new trip with valid information accurately, and redirect" do
+      fake_passenger = Passenger.create(name: 'CheezItMan', phone_num: '123-456-7890')
+      fake_driver = Driver.create(name: 'Devin', vin: 567, available: true)
 
       trip_info = {
-          trip: {
-              driver_id: driver.id,
-              passenger_id: pass.id,
-              date: Date.today,
-              cost: nil,
-              rating: nil
-          }
+        trip: {
+          driver_id: fake_driver.id,
+          passenger_id: fake_passenger.id,
+          date: Date.today,
+          cost: nil,
+          rating: nil
+        }
       }
 
       expect {
-        post passenger_trips_path(pass), params: trip_info
+        post passenger_trips_path(fake_passenger), params: trip_info
       }.must_differ "Trip.count", 1
 
       new_trip = Trip.find_by(passenger_id: trip_info[:trip][:passenger_id])
