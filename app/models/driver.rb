b92543d12c95c303336340884da81b2862c6029d
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-04 18:00:32 -0800

    added total_earnings and average_rating model definitions and view logic

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -5,6 +5,19 @@ class Driver < ApplicationRecord
 
   validates :vin, presence: true, uniqueness: true
 
+  def total_earnings
+
+    sum_revenue = self.trips.sum { |trip| trip.cost > 1.65 ? 0.8 * (trip.cost - 1.65) : trip.cost }
+
+    return sum_revenue.round(2)
+
+  end
+
+  def average_rating
+    total_trips = trips.length
+    return self.trips.sum { |trip| trip.rating.to_f }/total_trips
+  end
+
   def change_status
     if self.availability_status
       self.availability_status = false
