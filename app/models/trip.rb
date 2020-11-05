e5babfb5ecb7ea2ceac6b6cf3d81c76c05689fa7
Anna Kim <anna.kim93@gmail.com>
2020-11-05 12:23:50 -0500

    commented out validation for rating

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,5 +2,5 @@ class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
 
-  validates :rating, numericality: { only_integer: true, in: 1..5 }
+  # validates :rating, numericality: { only_integer: true, in: 1..5 }
 end
