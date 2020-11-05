e7b6e74d3f8146081a94786f91a18665f05b022c
Marj E <marj@marjorieetienne.com>
2020-11-05 06:20:41 -0800

    deleted trips index html file and changed routes for drivers

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to:'homepages#index'
 
-  resources :drivers do
-    resources :trips, only: [:show, :new]
-  end
-
   resources :passengers do
     resources :trips, only: [:show, :new]
   end
+
+  resources :drivers
 
   resources :trips
 end
