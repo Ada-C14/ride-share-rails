78115e1faa117952bd448157a025f8b41bd2ffd0
Noor B.A <noor.bahr.aluloom@gmail.com>
2020-11-05 14:36:56 -0800

    validation for trips,passeengers and driver

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,7 +1,7 @@
 class Passenger < ApplicationRecord
   has_many :trips
 
-  #validates :name, presence: true
-  #validates :phone_num, presence: true, numericality: { only_integer: true }, length: { is: 10}
+  validates :name, presence: true
+  validates :phone_num, presence: true
 
 end
--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,6 +2,7 @@ class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
 
-  # validates :rating, length: { in: 1..5}
+  # validates :rating, numericality: { only_integer: true, greater_than: 0 ,less_than: 6 }
   # validates :cost, numericality: { only_integer: true, greater_than: 0 }
+
 end
