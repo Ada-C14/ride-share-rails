fd795809ec47808ad2182bc98e022fbd6622313f
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-11-04 18:43:55 -0800

    add details to show page for driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -3,4 +3,33 @@ class Driver < ApplicationRecord
 
   validates :name, presence: true
   validates :vin, presence: true
+
+  def total_earnings
+    every_costs = self.trips.where.not(cost: nil)
+    sum = 0
+    every_costs.each do |trip|
+      if trip.cost <= 1.65 * 100
+        sum += trip.cost
+      else
+        sum += (trip.cost - 1.65 * 100) * 0.8
+      end
+    end
+
+    if sum
+      return (sum / 100).round(2)
+    else
+      return nil
+    end
+  end
+
+  def ave_rating
+    rating_by_passengers = self.trips.where.not(rating: nil)
+    sum = rating_by_passengers.sum { |trip| trip.rating}
+
+    if sum
+      return (sum / rating_by_passengers.count.to_f).round(1)
+    else
+      return nil
+    end
+  end
 end
