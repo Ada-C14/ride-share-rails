fb59f44dc7e32effc4811144b97e2f3d42549fbd
Leah Scott-Zechlin <scottzec@gmail.com>
2020-11-05 10:00:26 -0800

    form for edit and new working, but needs touching up

--- app/controllers/drivers_controller.rb
+++ app/controllers/drivers_controller.rb
@@ -3,11 +3,26 @@ class DriversController < ApplicationController
     @drivers = Driver.all
   end
 
+  def new
+    @driver = Driver.new
+  end
+
   def show
     driver_id = params[:id] #we'll be able to access our route parameter via a special hash provided by Rails called params. The ID sent by the browser will be stored under the key :id (remember that this is the name we gave the parameter in the routefile).
     @driver = Driver.find_by(id: driver_id)
     if @driver.nil?
       redirect_to drivers_path
+      return
+    end
+  end
+
+  def create
+    @driver = Driver.new(driver_params)
+    if @driver.save # save returns true if the database insert succeeds
+      redirect_to driver_path(@driver.id)
+      return
+    else
+      render :new
       return
     end
   end
@@ -38,6 +53,32 @@ class DriversController < ApplicationController
       return
     end
   end
+
+  def destroy
+    driver_id = params[:id]
+    @driver = Driver.find_by(id: driver_id)
+    @driver.destroy
+    redirect_to root_path
+  end
+
+  def availability
+    driver_id = params[:id]
+    @driver = Driver.find_by(id: driver_id)
+    if @driver.available.true
+      @driver.available = false
+    elsif @driver.available.false
+      @driver.available = true
+    end
+    @driver.save
+    redirect_to root_path
+  end
+
+  private
+
+  def driver_params
+    return params.require(:driver).permit(:name, :vin, :available)
+  end
+
 end
 
 
