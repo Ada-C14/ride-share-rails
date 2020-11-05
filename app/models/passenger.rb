c3f07ae204a2c01c30de062023d33aec6d7d7410
Anna Kim <anna.kim93@gmail.com>
2020-11-05 14:15:00 -0500

    adding Passenger#lifetime_cost to calc total spent on rides

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,9 @@ class Passenger < ApplicationRecord
   has_many :trips, dependent: :destroy
   validates :name, presence: true
   validates :phone_num, presence: true
+
+  def lifetime_cost
+    lifetime_cost = self.trips.sum(:cost)
+    return lifetime_cost
+  end
 end
