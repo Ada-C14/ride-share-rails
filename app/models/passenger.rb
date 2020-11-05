51ddb54c5f9e3d66a7e485d1609beaf4566ee136
Sophia Donan <sophiadonan@gmail.com>
2020-11-05 15:49:38 -0800

    completed passenger show page and created table for trips

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,7 +2,7 @@ class Passenger < ApplicationRecord
   has_many :trips
 
   def total_spent
-    total = self.trips.sum(:cost)
+    total = self.trips.sum(:cost)/100
     return total > 0 ? total : 0
   end
 end
