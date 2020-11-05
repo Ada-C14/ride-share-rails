62b0d3388fba8aff54b0ca790a58319fd91a838b
KayKay-git <kayalew@yahoo.com>
2020-11-04 19:02:51 -0500

    MERGE: Added action to trips  methods, commiing before merging

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,21 +1,43 @@
 class TripsController < ApplicationController
 
+  before_action :find_trip, except: [:index, :new, :create]
+
   def index
+    @trips = Driver.all.order(:date)
   end
 
   def show
+    if @trips.nil?
+      redirect_to trips_path
+      return
+    end
   end
 
   def new
-
+    @trip = Trip.new
   end
 
   def create
+    @trip = Trip.new(trip_params)
 
+    # save returns true if the database insert succeeds
+    if @trip.save
+      # go to the index so we can see the trip in the list, send them back to '/trips' path
+      redirect_to trip_path(@trip.id)
+      return
+    else
+    render :new, :bad_request
+    return
+    end
   end
 
   def edit
-
+    if @trip.nil?
+      redirect_to edit_trip_path
+      # we can redirect to drivers_path index
+      # or back to edit with friendly error message
+      return
+    end
   end
 
   def update
@@ -26,5 +48,13 @@ class TripsController < ApplicationController
 
   end
 
+  private
 
+  def find_trip
+    @trip = Trip.find_by(id: params[:id])
+  end
+
+  def trip_params
+    return params.require(:trip).permit(:date, :rating, :cost, :driver_id, :passenger_id)
+  end
 end
