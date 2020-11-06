db457344eff009c9d05244c9032743ac71250699
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-06 11:46:53 -0800

    Added tests for Driver.change_status

--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -127,13 +127,19 @@ describe Driver do
     end
 
     describe "can go online" do
-      # Your code here
+      it "changes an unavailable driver to available" do
+        offline_driver = Driver.create(name: "New Driver", vin: "00000000999", availability_status: false)
+        offline_driver.change_status
+        expect(offline_driver.availability_status).must_equal true
+      end
     end
 
     describe "can go offline" do
-      # Your code here
+      it "changes an available driver to unavailable" do
+        @new_driver.change_status
+        expect(@new_driver.availability_status).must_equal false
+      end
     end
 
-    # You may have additional methods to test
   end
 end
