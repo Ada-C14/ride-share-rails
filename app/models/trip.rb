5230175e15d3de44fe5adda4d40fbfc435fc9f07
Christabel Sebastian <christabel.escarez@gmail.com>
2020-11-05 19:54:43 -0800

    added validation to ensure rating is some integer between one and five

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,4 +4,5 @@ class Trip < ApplicationRecord
 
   validates :date, presence: true
   validates :cost, presence: true
+  validates :rating, numericality: {only_integer: true, greater_than: 0, less_than: 6}
 end
