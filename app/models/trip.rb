5ffaa9ee83201f16c8c856c2048b7f4e13810f97
Marj E <marj@marjorieetienne.com>
2020-11-05 19:06:11 -0800

    finished the driver tests

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,8 +3,8 @@ class Trip < ApplicationRecord
   belongs_to :driver
 
   # attribute :rating, default: rand(1...5)
-  validates :date, presence: true
-  validates :driver, presence: true
-  validates :passenger, presence: true
-  validates :cost, presence: true
+  validates :date, presence: { message: "can't be blank" }
+  validates :driver, presence: { message: "can't be blank" }
+  validates :passenger, presence: { message: "can't be blank" }
+  validates :cost, presence: { message: "can't be blank" }
 end
--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -43,19 +43,35 @@ describe Trip do
 
   describe "validations" do
     it "must have a date" do
+      new_trip.date = nil
 
+      expect(new_trip.valid?).must_equal false
+      expect(new_trip.errors.messages).must_include :date
+      expect(new_trip.errors.messages[:date]).must_equal ["can't be blank"]
     end
 
     it "must have a driver" do
+      new_trip.driver = nil
 
+      expect(new_trip.valid?).must_equal false
+      expect(new_trip.errors.messages).must_include :driver
+      expect(new_trip.errors.messages[:driver]).must_equal ["must exist", "can't be blank"]
     end
 
     it "must have a passenger" do
+      new_trip.passenger = nil
 
+      expect(new_trip.valid?).must_equal false
+      expect(new_trip.errors.messages).must_include :passenger
+      expect(new_trip.errors.messages[:passenger]).must_equal ["must exist", "can't be blank"]
     end
 
     it "must have a cost" do
+      new_trip.cost = nil
 
+      expect(new_trip.valid?).must_equal false
+      expect(new_trip.errors.messages).must_include :cost
+      expect(new_trip.errors.messages[:cost]).must_equal ["can't be blank"]
     end
   end
 
