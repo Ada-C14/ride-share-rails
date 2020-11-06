c374fdde5e4264a455d7e1aa95caf119bb663f09
r-spiel <richelle.mehlhaff@gmail.com>
2020-11-06 11:11:05 -0800

    update homepage index and controller fix printing of trips total and add revenue stat

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,25 +1,53 @@
 class HomepagesController < ApplicationController
 
   def index
-    drivers = Driver.all #would it better to write these in a driver model method etc.?
-    @driver_count = drivers.size
-    @available_drivers = drivers.where(available: true).size
-    @unavailable_drivers = drivers.where(available: false).size
-
+    drivers = Driver.all #would it better to write these in a driver model method etc.?  Probably too much logic should move to Driver model
+    if drivers.empty?
+      @driver_count = 0
+      @available_drivers = "N/A"
+      @unavailable_drivers = "N/A"
+    else
+      @driver_count = drivers.size
+      @available_drivers = drivers.where(available: true).size
+      @unavailable_drivers = drivers.where(available: false).size
+    end
 
     passengers = Passenger.all
-    @passenger_count = passengers.size
+    if passengers.empty?
+      @passenger_count = 0
+    else
+      @passenger_count = passengers.size
+    end
 
     trips = Trip.all
-    @trip_count = trips.size
-
-    ratings = []
-    trips.each do |trip|
-      unless trip.rating.nil?
-        ratings << trip.rating
-      end
+    if trips.empty?
+      @trip_count = 0
+    else
+      @trip_count = trips.size
     end
-    @average_rating = (ratings.sum) / ratings.length
+    #@trip_count = 0
+
+    #should move this logic to trips model?
+    ratings = []
+    if @trip_count == 0
+      @average_rating = "Not available"
+    else
+      trips.each do |trip|
+        unless trip.rating.nil?
+          ratings << trip.rating
+        end
+      end
+      @average_rating = (ratings.sum) / ratings.length
+    end
+
+    @total_revenue = 0
+    if @trip_count == 0
+      @total_revenue = "N/A"
+    else
+      trips.each {|trip| @total_revenue += trip.cost }
+    end
+
+
   end
     
     #@ave_trip_rating = trips.size
