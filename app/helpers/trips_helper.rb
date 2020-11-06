6b5a145f2e0649411c3854757cfa2eea8cb3ecd7
Rachael Gomez <gomezrc1220@gmail.com>
2020-11-06 13:10:29 -0800

    made helper methods for adding a new trip and fixed where new trip linked to in passenger index

--- app/helpers/trips_helper.rb
+++ app/helpers/trips_helper.rb
@@ -1,2 +1,14 @@
 module TripsHelper
+  def request_trip
+    assigned_driver = @drivers.find { |driver| driver.available == true }
+    if assigned_driver.nil?
+      return nil
+    end
+    
+    def random_cost
+      cost = random(12...30)
+      cost = cost.to_f / 100
+      return cost
+    end
+    
 end
