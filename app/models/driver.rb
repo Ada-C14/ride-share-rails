97c5aa89db333cde3bb053956c8133975759ebb6
Stacy <lundquist.stacy@gmail.com>
2020-11-05 21:50:54 -0800

    driver.rb#first_available_driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,36 +1,49 @@
 class Driver < ApplicationRecord
   has_many :trips
 
-  def average_rating
-    total = 0
-    complete_trip = 0
-    trips.compact.each do |trip|
-      unless trip.rating.nil?
-        total += trip.rating.to_f
-        complete_trip += 1
+  def first_available_driver
+    first_available_driver = nil
+    drivers.each do |driver|
+      if driver.availability == :AVAILABLE
+        first_available_driver = driver
+        break
       end
     end
 
-    if complete_trip == 0
-      return 0
+    if first_available_driver == nil
+      return "No available drivers"
     end
 
-    average_total = total / complete_trip
-    return average_total
-  end
-
-  def total_revenue
-    revenue = 0
-
-    trips.each do |trip|
-      revenue +=
-        if trip.cost.to_f > 1.65
-          (trip.cost.to_f - 1.65) * 0.8
-        else
-          (trip.cost.to_f) * 0.8
+    def average_rating
+      total = 0
+      complete_trip = 0
+      trips.compact.each do |trip|
+        unless trip.rating.nil?
+          total += trip.rating.to_f
+          complete_trip += 1
         end
+      end
+
+      if complete_trip == 0
+        return 0
+      end
+
+      average_total = total / complete_trip
+      return average_total
     end
 
-    return revenue
+    def total_revenue
+      revenue = 0
+
+      trips.each do |trip|
+        revenue +=
+            if trip.cost.to_f > 1.65
+              (trip.cost.to_f - 1.65) * 0.8
+            else
+              (trip.cost.to_f) * 0.8
+            end
+      end
+
+      return revenue
+    end
   end
-end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,5 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+
 end
