08edd4334066338dc7f31787c7e954936e71e50d
Alice Boone <alicegbonel@gmail.com>
2020-11-05 17:15:56 -0800

    Fixing bugs on the views

--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -1,13 +1,42 @@
 # require "test_helper"
 #
-#   describe "relationships" do
-#     # Your tests go here
-#   end
-#
-#   describe "validations" do
-#     # Your tests go here
-#   end
-#
+  describe "relationships" do
+    it "can have many trips" do
+      # Arrange
+      new_passenger.save
+      new_driver = Driver.create(name: "Waldo", vin: "ALWSS52P9NEYLVDE9")
+      trip_1 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 5, cost: 1234)
+      trip_2 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 3, cost: 6334)
+
+      # Assert
+      expect(new_passenger.trips.count).must_equal 2
+      new_passenger.trips.each do |trip|
+        expect(trip).must_be_instance_of Trip
+      end
+    end
+  end
+
+  describe "validations" do
+    it "must have a name" do
+      # Arrange
+      new_passenger.name = nil
+
+      # Assert
+      expect(new_passenger.valid?).must_equal false
+      expect(new_passenger.errors.messages).must_include :name
+      expect(new_passenger.errors.messages[:name]).must_equal ["can't be blank"]
+    end
+
+    it "must have a phone number" do
+      # Arrange
+      new_passenger.phone_num = nil
+
+      # Assert
+      expect(new_passenger.valid?).must_equal false
+      expect(new_passenger.errors.messages).must_include :phone_num
+      expect(new_passenger.errors.messages[:phone_num]).must_equal ["can't be blank"]
+    end
+  end
 #   # Tests for methods you create should go here
 #   describe "custom methods" do
 #     # Your tests here
