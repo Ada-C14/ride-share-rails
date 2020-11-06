bfea8940bca9feee922e21cf7272a2c0ac08991c
Anna Kim <anna.kim93@gmail.com>
2020-11-06 01:58:23 -0500

    added trip model validation tests

--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -37,14 +37,47 @@ describe Trip do
   end
 
   describe "validations" do
-    it "must have a rating between 1 and 5" do
+    it "must have a rating between 1 and 5 on update" do
       # Arrange
-      # new_passenger.name = nil
+      driver
+      trip.rating = -5
 
       # Assert
-      # expect(new_passenger.valid?).must_equal false
-      # expect(new_passenger.errors.messages).must_include :name
-      # expect(new_passenger.errors.messages[:name]).must_equal ["can't be blank"]
+      expect(trip.valid?).must_equal false
+      expect(trip.errors.messages).must_include :rating
+      expect(trip.errors.messages[:rating]).must_equal ["must be greater than or equal to 0",
+                                                        "is not included in the list"]
+    end
+
+    it "must have a date" do
+      driver
+      trip.date = nil
+
+      expect(trip.valid?).must_equal false
+      expect(trip.errors.messages).must_include :date
+    end
+
+    it "must have a cost greater than 0" do
+      driver
+      trip.cost = -99.2
+
+      expect(trip.valid?).must_equal false
+      expect(trip.errors.messages).must_include :cost
+    end
+
+    it "must have a driver_id" do
+      trip
+
+      expect(trip.valid?).must_equal false
+      expect(trip.errors.messages).must_include :driver_id
+    end
+
+    it "must have a passenger_id" do
+      driver
+      trip.passenger_id = nil
+
+      expect(trip.valid?).must_equal false
+      expect(trip.errors.messages).must_include :passenger_id
     end
   end
 
