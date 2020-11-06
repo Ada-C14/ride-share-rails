38219f16fea2d736bdd2cd92b0cdfc5876df9d66
Marj E <marj@marjorieetienne.com>
2020-11-06 05:08:11 -0800

    added money gem and passenger total_cost method

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,5 +1,15 @@
+require 'money'
+
 class Passenger < ApplicationRecord
   has_many :trips
   validates :name, presence: true
   validates :phone_num, presence: true
+
+  def total_cost
+    total_cost = 0
+    self.trips.each do |trip|
+      total_cost += trip.cost
+    end
+    return Money.new(total_cost, "USD").format
+  end
 end
