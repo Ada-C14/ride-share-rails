824434ab84fe5f55a4a0aed7c024e3be6d1c37fb
OlgaSe <olga.tka4eva@gmail.com>
2020-11-05 18:07:05 -0800

    Statistics: total revenue for a driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,3 +1,17 @@
 class Driver < ApplicationRecord
   has_many :trips
+
+
+  def earned
+    sum = 0
+    trips.each do |trip|
+      if trip.cost < 0
+        return 0
+      else
+        sum += (trip.cost - 165) * 0.8
+      end
+    end
+    return sum.round(2)
+  end
+
 end
