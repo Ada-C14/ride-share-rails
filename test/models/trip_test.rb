94c123e1c746eff222fabe822b01439211e169c5
Yingjing Lin <yjlin789@gmail.com>
2020-11-05 23:58:11 -0800

    pass tests for model trip & passenger

--- test/models/trip_test.rb
+++ test/models/trip_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 describe Trip do
+  before do
+    Passenger.create(name: "Banana", phone_num: "123-123-1230")
+    Driver.create(name: "Berry", vin:"123456780", available:true )
+  end
+
   let (:new_trip) {
-    Trip.new(driver_id: 1, passenger_id: 1, date: Date.today, rating: 3, cost: 1023)
+    Trip.new(driver_id: Driver.first.id, passenger_id: Passenger.first.id, date: Date.today, rating: 3, cost: 1023)
   }
   it "can be instantiated" do
     expect(new_trip.valid?).must_equal true
@@ -29,7 +34,7 @@ describe Trip do
 
       expect(new_trip.valid?).must_equal false
       expect(new_trip.errors.messages).must_include :driver_id
-      expect(new_trip.errors.messages[:driver_id]).must_equal ["can't be blank"]
+      expect(new_trip.errors.messages[:driver_id]).must_equal ["can't be blank", "is not a number"]
     end
 
     it "must have a passenger" do
@@ -37,12 +42,19 @@ describe Trip do
 
       expect(new_trip.valid?).must_equal false
       expect(new_trip.errors.messages).must_include :passenger_id
-      expect(new_trip.errors.messages[:passenger_id]).must_equal ["can't be blank"]
+      expect(new_trip.errors.messages[:passenger_id]).must_equal ["can't be blank", "is not a number"]
     end
   end
 
   # Tests for methods you create should go here
-  describe "custom methods" do
-    # Your tests here
+  describe "request_trip" do
+    it "will create a trip but not save to database" do
+      Passenger.create(name: "Test", phone_num: "123-123-1234")
+      Driver.create(name: "Peach", vin:"123456789", available:true )
+
+      trip = Trip.request_trip(Passenger.last.id)
+      expect { Trip.request_trip(Passenger.last.id)}.wont_change 'Trip.count'
+      expect(trip.passenger_id).must_equal Passenger.last.id
+    end
   end
 end
