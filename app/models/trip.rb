aeba882574dd110754854221b1edc028a58e2c87
Anna Kim <anna.kim93@gmail.com>
2020-11-05 20:20:58 -0500

    moving business logic to trip model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,4 +3,18 @@ class Trip < ApplicationRecord
   belongs_to :passenger
 
   validates :rating, :inclusion => { :in => 1..5 }, on: :update
+
+  def self.generate_cost
+    return rand(8.0..35.0).round(2)
+  end
+
+  def self.assign_driver
+    return Driver.find_by(available: 'true').id
+  end
+
+  def change_driver_status
+    driver = Driver.find_by(id: self.driver_id)
+    driver.available = 'false'
+    driver.save
+  end
 end
