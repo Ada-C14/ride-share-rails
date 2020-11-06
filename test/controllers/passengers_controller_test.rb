6ea8b1efbed1b792e32963952060147670eaf405
Mackenzie <mvlofthus@gmail.com>
2020-11-05 16:38:01 -0800

    starting testing, trips controller test for show does not work

--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -1,16 +1,72 @@
 require "test_helper"
 
 describe PassengersController do
+
+  let (:passenger) {
+    Passenger.create(name: "Test Passenger", phone_num: "2065550000")
+  }
+
   describe "index" do
     # Your tests go here
+    it "responds with success when there are many passengers saved" do
+      # Arrange
+      # Ensure that there is at least one Passenger saved
+      passenger
+      expect(Passenger.count).must_equal 1
+
+      # Act
+      get passengers_path
+
+      # Assert
+      must_respond_with :success
+    end
+
+    it "responds with success when there are no passenger saved" do
+      # Arrange
+      # Ensure that there are zero passengers saved
+      expect(Passenger.count).must_equal 0
+
+      # Act
+      get passengers_path
+
+      # Assert
+      must_respond_with :success
+    end
   end
 
   describe "show" do
     # Your tests go here
+    it "responds with success when showing an existing valid passenger" do
+      # Arrange
+      # Ensure that there is a passenger saved
+
+      # Act
+      get passenger_path(passenger.id)
+
+      # Assert
+      must_respond_with :success
+    end
+
+    it "responds with redirect with an invalid passenger id" do
+      # Arrange
+      # Ensure that there is an id that points to no passenger
+
+      # Act
+      get passenger_path(-1)
+
+      # Assert
+      must_respond_with :redirect
+      must_redirect_to passengers_path
+    end
   end
 
   describe "new" do
     # Your tests go here
+    it "responds with success" do
+      get new_passenger_path
+
+      must_respond_with :success
+    end
   end
 
   describe "create" do
--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -1,8 +1,35 @@
 require "test_helper"
 
 describe TripsController do
+  before do
+      Driver.create(name: "Test Driver", vin: "12345678912345678", available: true)
+      Passenger.create(name: "Test Passenger", phone_num: "2065550000")
+  end
+
+  let (:trip) {
+    Trip.create(date: "today",
+                rating: nil,
+                cost: 2560,
+                passenger_id: Passenger.first.id,
+                driver_id: Driver.first.id)
+  }
+
   describe "show" do
     # Your tests go here
+    # it "responds with success when showing an existing valid trip" do
+    #   trip
+    #   get trip_path(trip.id)
+    #
+    #   must_respond_with :success
+    # end
+
+    it "responds with redirect with an invalid trip id" do
+      get trip_path(-1)
+
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
+
   end
 
   describe "create" do
