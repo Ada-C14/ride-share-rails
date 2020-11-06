3730c6555178b7dc1c44044f39b625941bf766a2
OlgaSe <olga.tka4eva@gmail.com>
2020-11-05 18:15:39 -0800

    Statistics: avarage rating for a driver

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -14,4 +14,16 @@ class Driver < ApplicationRecord
     return sum.round(2)
   end
 
+  def average_rating
+    average = 0
+    sum = 0
+    count = 0
+    trips.each do |trip|
+      sum += trip.rating
+      count += 1
+    end
+    average = (sum / count.to_f).round(2)
+    return average
+  end
+
 end
