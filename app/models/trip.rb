a1d5ee0fd3b9f7b42b8eb1e6868957178d40d2f8
Stacy <lundquist.stacy@gmail.com>
2020-11-06 11:32:35 -0800

    fixed merge conflicts

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,7 +2,6 @@ class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
 
-
   validates :date, presence: true
 
   validates :rating, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 5}
