d590d46e310b895f179bd4cb25e290bf788bbbe1
OlgaSe <olga.tka4eva@gmail.com>
2020-11-05 18:20:48 -0800

    Statistics: Total spent for passenger

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,11 @@
 class Passenger < ApplicationRecord
   has_many :trips
+
+  def total_spent
+    total_spent = 0
+    trips.each do |trip|
+      total_spent += trip.cost
+    end
+    return total_spent
+  end
 end
