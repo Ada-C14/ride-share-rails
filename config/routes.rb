c4c6d231ca5076abe479e30009b204810f951481
Noor B.A <noor.bahr.aluloom@gmail.com>
2020-11-05 22:53:49 -0800

    updated routes with the copied one

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :drivers
-
   resources :trips, except: [:create]
-
   resources :passengers do
     resources :trips, only: [:index, :create]
   end
-
   delete '/drivers/:id', to: 'drivers#delete', as: 'delete_driver'
-
+  #root to: do the root
 end
