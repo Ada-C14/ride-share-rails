fac6ba0204afc1187504fefe875198daada8e15d
Kareha Agesa <kmaagesa@gmail.com>
2020-11-05 20:13:02 -0800

    example code from becca

---
+++ temps.rb
@@ -0,0 +1,27 @@
+# create method in trips
+# passenger/:passenger_id/trip - will be route for create
+def create
+  driver = Driver.find_by(:available = true)
+  passenger = Passenger.find_by(id: params[:passenger_id])
+  @trip = Trip.new(
+    driver_id = driver.id,
+    passenger_id = passenger.id
+  )
+end
+
+# in the routes
+resources :passengers do
+  resources :trips, only [:create]
+
+  post passenger/:passenger_id/trip, to trips#create, as: passenger_trips
+
+# can set driver as unavilable in trips#create too 
+# user action to set driver as unavailable 
+# patch driver/:id/set_unavailable to: driver#unavailable, as: set_unavailable
+# driver_controller
+def set_unavailable
+  # maybe a find by here? idk girl. if driver.nil? redirect, else mark unavailable
+  driver.mark_unavailable
+end
+
+
