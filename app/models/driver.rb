fbf24d40d7fe0eec1244570eabd4f43980ebfaa2
Jessica Chan <jwinchan@gmail.com>
2020-11-06 05:13:14 -0800

    added validation to drivers and error messages in form

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,6 +1,9 @@
 class Driver < ApplicationRecord
   has_many :trips
 
+  validates :name, presence: true
+  validates :vin, presence: true
+
   def mark_unavailable
     self.available = false
     self.save
