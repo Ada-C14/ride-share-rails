bf62c1ae94c42e84125c9afb3580d031d22d2085
Stacy <lundquist.stacy@gmail.com>
2020-11-05 17:13:56 -0800

    driver model #total_rev, avg_total

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,5 +1,36 @@
 class Driver < ApplicationRecord
   has_many :trips
 
+  def average_rating
+    total = 0
+    complete_trip = 0
+    trips.compact.each do |trip|
+      unless trip.rating.nil?
+        total += trip.rating.to_f
+        complete_trip += 1
+      end
+    end
 
+    if complete_trip == 0
+      return 0
+    end
+
+    average_total = total / complete_trip
+    return average_total
+  end
+
+  def total_revenue
+    revenue = 0
+
+    trips.each do |trip|
+      revenue +=
+        if trip.cost.to_f > 1.65
+          (trip.cost.to_f - 1.65) * 0.8
+        else
+          (trip.cost.to_f) * 0.8
+        end
+    end
+
+    return revenue
+  end
 end
