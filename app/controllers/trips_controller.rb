e827fab3041a40d118eb87896071f58d32fc795a
Kareha Agesa <kmaagesa@gmail.com>
2020-11-05 22:12:35 -0800

    a hopeless trips#show method

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,20 +1,21 @@
 class TripsController < ApplicationController
     
-  def index
-    if params[:driver_id]
-      # This is the nested route, /driver/:driver_id/trips
-      driver = Driver.find_by(id: params[:driver_id])
-      @trips = driver.trips
-    elsif params[:passenger_id]
-      passenger = Passenger.find_by(id: params[:passenger_id]) 
-      @trips = passenger.trips
-    else
-      # This is the 'regular' route, /trips
-      @trips = Trip.all
-    end
-  end
+  # def index
+  #   if params[:driver_id]
+  #     # This is the nested route, /driver/:driver_id/trips
+  #     driver = Driver.find_by(id: params[:driver_id])
+  #     @trips = driver.trips
+  #   elsif params[:passenger_id]
+  #     passenger = Passenger.find_by(id: params[:passenger_id]) 
+  #     @trips = passenger.trips
+  #   else
+  #     # This is the 'regular' route, /trips
+  #     @trips = Trip.all
+  #   end
+  # end
 
   def show # details of an instance of an object
+    raise
 
     if params[:driver_id]
       driver = Driver.find_by(id: params[:driver_id])
