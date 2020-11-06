db64962bd0a2bf9eff699d08649d2d54a69f263b
Hanh Solo <minhhanhnyc@gmail.com>
2020-11-05 21:38:48 -0800

    Updated trip class

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,35 @@
 class TripsController < ApplicationController
+    
+  def index
+    if params[:driver_id]
+      # This is the nested route, /driver/:driver_id/trips
+      driver = Driver.find_by(id: params[:driver_id])
+      @trips = driver.trips
+    elsif params[:passenger_id]
+      passenger = Passenger.find_by(id: params[:passenger_id]) 
+      @trips = passenger.trips
+    else
+      # This is the 'regular' route, /trips
+      @trips = Trip.all
+    end
+  end
+
+  def show # details of an instance of an object
+
+    if params[:driver_id]
+      driver = Driver.find_by(id: params[:driver_id])
+      raise
+      trip = driver.trips.find_by(id: 124)
+    end
+
+
+    passenger_id = params[:id]
+    @passenger = Passenger.find_by(id: passenger_id)
+
+    if @passenger.nil?
+      redirect_to passengers_path
+      return
+    end
+end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   resources :drivers
   resources :passengers
   resources :trips
+
   
+
+  get '/drivers/:driver_id/trips', to: 'trips#index', as: 'driver_trips'
+  get '/passengers/:passenger_id/trips', to: 'trips#index', as: 'passenger_trips'
+  get '/drivers/:driver_id/trips/:trip_id', to: 'trips#show'
+  get '/passengers/:passenger_id/trips/:trip_id', to: 'trips#show'
   root to: 'homepages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
