9ff8db399599658d7a68bc6da701357c81cb1ecb
Iris Lux <iris.lux0@gmail.com>
2020-11-05 18:32:03 -0800

    total_earnings method

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -2,10 +2,15 @@ class Driver < ApplicationRecord
   has_many :trips
 
   validates :name, presence: true
-  validates :vin, presence: true, length: { is: 17 }
+  validates :vin, presence: true #, length: { is: 17 }
 
   def toggle_available
     self.update(available: !self.available)
   end
 
+  def total_earnings
+    gross_earnings = self.trips.map{|trip| trip.cost}.sum
+
+    return gross_earnings > 165 ? ((gross_earnings - 1.65) * 0.8).round : 0
+  end
 end
