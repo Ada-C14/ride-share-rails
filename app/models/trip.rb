1c4c0a994ae07b15ef23c95b9b8bf35b0550e5c2
Iris Lux <iris.lux0@gmail.com>
2020-11-06 00:00:33 -0800

    added validations, for rating and cost

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,7 +2,8 @@ class Trip < ApplicationRecord
   belongs_to :driver
   belongs_to :passenger
 
-  # validates :rating, numericality: { only_integer: true, greater_than: 0 ,less_than: 6 }
-  # validates :cost, numericality: { only_integer: true, greater_than: 0 }
+  validates :date, presence: true
+  validates :rating, numericality: { only_integer: true, greater_than: 0 ,less_than: 6 }, allow_nil: true
+  validates :cost, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
 
 end
