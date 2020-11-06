a0b159c6ca4cc929ec08458dc59351213dfb536d
Hanh Solo <minhhanhnyc@gmail.com>
2020-11-05 16:56:41 -0800

    updated passengers testing

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -2,4 +2,13 @@ class Passenger < ApplicationRecord
     has_many :trips
     validates :phone_num, numericality: { only_integer: true }
     validates :name, presence: true
+
+    def net_expenditures
+        total = 0
+        self.trips.each do |trip|
+          total += trip.cost 
+        end
+        return total
+    end
+
 end
