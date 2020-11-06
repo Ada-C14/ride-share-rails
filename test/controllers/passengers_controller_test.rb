d0db08d8dbaef0619d104ae98738d982d4dd9d75
Maha-ElMais <mahaelmais@gmail.com>
2020-11-06 10:51:16 -0800

    passenger validation

--- test/controllers/passengers_controller_test.rb
+++ test/controllers/passengers_controller_test.rb
@@ -1,9 +1,20 @@
 require "test_helper"
 
 describe PassengersController do
-  describe "index" do
-    # Your tests go here
+  before do
+    @passenger = Passenger.new(name: "hello world", phone_num: "123 444 2129")
   end
+  describe "index" do
+    it "can get the index path" do
+
+      get passengers_path #resources :passengers
+      must_respond_with :success
+    end
+    it "can get the root path" do
+
+      get root_path
+      must_respond_with :success
+    end
 
   describe "show" do
     # Your tests go here
@@ -28,4 +39,5 @@ describe PassengersController do
   describe "destroy" do
     # Your tests go here
   end
-end
+  end
+  end
