ffa0d7d3237b178918bb4f2a0bf76974546e1521
r-spiel <richelle.mehlhaff@gmail.com>
2020-11-05 22:26:18 -0800

    update homepage controller and homepage index.html

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,2 +1,31 @@
 class HomepagesController < ApplicationController
+
+  def index
+    drivers = Driver.all #would it better to write these in a driver model method etc.?
+    @driver_count = drivers.size
+    @available_drivers = drivers.where(available: true).size
+    @unavailable_drivers = drivers.where(available: false).size
+
+
+    passengers = Passenger.all
+    @passenger_count = passengers.size
+
+    trips = Trip.all
+    @trip_count = trips.size
+
+    ratings = []
+    trips.each do |trip|
+      unless trip.rating.nil?
+        ratings << trip.rating
+      end
+    end
+    @average_rating = (ratings.sum) / ratings.length
+  end
+    
+    #@ave_trip_rating = trips.size
+    # # Sum some numbers
+    # (5..10).reduce(:+)                             #=> 45
+    # # Same using a block and inject
+    # (5..10).inject { |sum, n| sum + n }
+
 end
