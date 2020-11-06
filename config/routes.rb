d1727cbd8bbeb41ecaf4b4ad15fd03c8ed7c958b
Iris Lux <iris.lux0@gmail.com>
2020-11-06 11:26:55 -0800

    added custome assign_rating route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :drivers
   resources :homepages, only: [:index]
-  resources :trips, except: [:create]
+  resources :trips, except: [:create] do
+    member do
+      patch :assign_rating
+    end
+  end
   resources :passengers do
     resources :trips, only: [:index, :create]
   end
