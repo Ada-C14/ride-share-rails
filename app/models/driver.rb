1893083b67ae681ca15e5bf4cd0856d165dbeca6
Madeline <madelign@gmail.com>
2020-11-06 19:25:58 -0500

    added business logic for driver and passenger

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -8,4 +8,24 @@ class Driver < ApplicationRecord
     self.available = false
     self.save
   end
+
+  # calculate total earnings
+  def total_earnings
+    if self.trips.empty?
+      return 0
+    end
+
+    revenue = self.trips.map { |trip| (trip[:cost] / 100.0 - 1.65) * 0.8 }
+    total = revenue.sum
+
+    return total
+  end
+
+  # calculate average rating
+  def average_rating
+    ratings = self.trips.map { |trip| trip[:rating] }
+    mean = (ratings.sum.to_f) / (ratings.length)
+
+    return mean
+  end
 end
--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -3,4 +3,17 @@ class Passenger < ApplicationRecord
 
   validates :name, presence: true
   validates :phone_num, presence: true
+
+    # calculate total charges
+    def total_charges
+      if self.trips.empty?
+        return 0
+      end
+  
+      revenue = self.trips.map { |trip| (trip[:cost] / 100.0 ) }
+      total = revenue.sum
+  
+      return total
+    end
+
 end
