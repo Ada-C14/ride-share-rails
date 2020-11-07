7a3a459c09d15bb76c814039ad1720eea745f8f6
Hanh Solo <minhhanhnyc@gmail.com>
2020-11-06 16:36:57 -0800

    updated trip controller test

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -1,28 +1,5 @@
 require "test_helper"
 
-# describe TripsController do
-#   skip
-#   describe "show" do
-#     # Your tests go here
-#   end
-
-#   describe "create" do
-#     # Your tests go here
-#   end
-
-#   describe "edit" do
-#     # Your tests go here
-#   end
-
-#   describe "update" do
-#     # Your tests go here
-#   end
-
-#   describe "destroy" do
-#     # Your tests go here
-#   end
-# end
-
 describe TripsController do
   before do
     @driver = Driver.create!(name: "Test Driver", vin: "123456", available: true)
