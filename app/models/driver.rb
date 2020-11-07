fc8bea40cb41cf77ec3a7853575ba4a7fd63abad
Mackenzie <mvlofthus@gmail.com>
2020-11-06 16:02:55 -0800

    driver_test passes

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -5,11 +5,15 @@ class Driver < ApplicationRecord
   validates :vin, presence: true, uniqueness: true, length: { is: 17 }
 
   def total_earnings
+    return 0 if self.trips.empty?
+
     costs = self.trips.filter.sum { |trip| trip.cost && trip.cost > 165 ? 0.8 * (trip.cost - 165) : 0 }
     return costs
   end
 
   def avg_rating
+    return 0 if self.trips.empty?
+
     valid_ratings = self.trips.filter { |trip| trip.rating }
     sum_ratings = valid_ratings.sum(0.0) { |trip| trip.rating }
     return (sum_ratings / valid_ratings.length).round(1)
--- test/models/driver_test.rb
+++ test/models/driver_test.rb
@@ -24,9 +24,9 @@ describe Driver do
     it "can have many trips" do
       # Arrange
       new_driver.save
-      new_passenger = Passenger.create(name: "Kari", phone_num: "111-111-1211")
-      trip_1 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 5, cost: 1234)
-      trip_2 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 3, cost: 6334)
+      new_passenger = Passenger.create!(name: "Kari", phone_num: "111-222-3333")
+      trip_1 = Trip.create!(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 5, cost: 1234)
+      trip_2 = Trip.create!(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 3, cost: 6334)
 
       # Assert
       expect(new_driver.trips.count).must_equal 2
@@ -60,32 +60,63 @@ describe Driver do
 
   # Tests for methods you create should go here
   describe "custom methods" do
+    before do
+      new_driver.save
+      new_passenger = Passenger.create!(name: "Kari Passenger", phone_num: "222-333-4444")
+      trip_1 = Trip.create!(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today.to_s, rating: 5, cost: 1234)
+      trip_2 = Trip.create!(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today.to_s, rating: 3, cost: 6334)
+    end
+
     describe "average rating" do
-      # Your code here
-      # new_driver = Driver.create(name: "Test", vin: "12345678912345678", available: true)
-      # trip_3 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: 4, cost: 1234)
-      # trip_4 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today, rating: nil, cost: 6334)
-
       it "returns float within range of 1.0 to 5.0" do
-        new_driver = Driver.new(name: "Kari Driver", vin: "12345678912345678", available: true)
-        new_driver.save
-        new_passenger = Passenger.create(name: "Kari Passenger", phone_num: "111-111-1211")
-        trip_1 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today.to_s, rating: 5, cost: 1234)
-        trip_2 = Trip.create(driver_id: new_driver.id, passenger_id: new_passenger.id, date: Date.today.to_s, rating: 3, cost: 6334)
-
-
         average = new_driver.avg_rating
         expect(average).must_be_kind_of Float
-        expect(new_driver.trips.count).must_equal 2
-        # expect(average).must_be :>=, 1.0
-        # expect(average).must_be :<=, 5.0
+        expect(average).must_be :>=, 1.0
+        expect(average).must_be :<=, 5.0
+        expect(average).must_equal 4.0
       end
 
+      it "successfully ignores in-progress trips with no rating, and doesn't alter average" do
+        new_p2 = Passenger.create!(name: "Second Passenger", phone_num: "333-444-5555")
+        trip_3 = Trip.create!(driver_id: new_driver.id, passenger_id: new_p2.id, date: Date.today, rating: 4, cost: 1234)
+        trip_4 = Trip.create!(driver_id: new_driver.id, passenger_id: new_p2.id, date: Date.today, rating: nil, cost: 6334)
+
+        average = new_driver.avg_rating
+        expect(average).must_equal 4.0
+      end
+
+      it "returns 0 if no trips" do
+        d1 = Driver.create(name: "Test No Trips", vin: "12345678912345600", available: true)
+
+        expect(d1.avg_rating).must_equal 0
+      end
 
     end
 
     describe "total earnings" do
-      # Your code here
+      it "returns float if trips" do
+        revenue = new_driver.total_earnings
+        expect(revenue).must_be_kind_of Float
+      end
+
+      it "returns 0 if no trips" do
+        d1 = Driver.create(name: "Test No Trips", vin: "12345678912345600", available: true)
+
+        expect(d1.total_earnings).must_equal 0
+      end
+
+      it "correctly calculates total revenue" do
+        revenue = new_driver.total_earnings
+        expect(revenue).must_be_close_to((1234-165 + 6334-165)*0.8)
+      end
+
+      it "returns 0 if trip cost <= 1.65" do
+        d1 = Driver.create(name: "Test No Trips", vin: "12345678912345600", available: true)
+        p1 = Passenger.create!(name: "Second Passenger", phone_num: "333-444-5555")
+        trip_1 = Trip.create!(driver_id: d1.id, passenger_id: p1.id, date: Date.today, rating: 4, cost: 50)
+
+        expect(d1.total_earnings).must_equal 0
+      end
     end
 
     # describe "can go online" do  -- DID NOT MAKE THESE AS METHODS --
