2cfe56eac0c5f7b892adf1dcdbd46c2d5e91de82
Blaine <b.atsbeha@gmail.com>
2020-11-07 20:19:00 -0800

    fixed the fix 549

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,4 +4,17 @@ class Trip < ApplicationRecord
 
   validates :driver_id, :passenger_id, :date, :cost, presence: true
   validates :cost, numericality: true
+
+  def total_earnings
+    return 0 if self.trips.empty?
+    sum = 0
+    trips.each do |trip|
+      if trip.cost < 0
+        return 0
+      else
+        sum += (trip.cost - 165) * 0.8
+      end
+    end
+    return sum
+  end
 end
