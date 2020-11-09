f0002866e2ed2586126671da4a818704dd1d6491
Schanen Ryan <schanen.r@gmail.com>
2020-11-08 16:52:35 -0800

    TRIPS: Added CSS for trip details and edit pages.

--- test/controllers/drivers_controller_test.rb
+++ test/controllers/drivers_controller_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 describe DriversController do
   # Note: If any of these tests have names that conflict with either the requirements or your team's decisions, feel empowered to change the test names. For example, if a given test name says "responds with 404" but your team's decision is to respond with redirect, please change the test name.
-  let (:driver) {
+  let(:driver) {
     Driver.create name: 'test_driver0', vin: 'ABCDEFGHIJKLMN0', available: true
   }
 
