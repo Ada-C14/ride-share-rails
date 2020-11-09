900892b97f92bf949dc82ddbc2e592205ba8f00e
Madeline <madelign@gmail.com>
2020-11-08 23:06:34 -0500

    added destroy test

--- test/controllers/trips_controller_test.rb
+++ test/controllers/trips_controller_test.rb
@@ -32,28 +32,48 @@ describe TripsController do
   end
 
   describe "create" do
- 
   end
 
   describe "edit" do
-    it "responds with success when getting the edit a trip" do
-      # Arrange
-      # Ensure there is an existing passenger saved
-
-      # Act
-      get edit_trip_path(@trip.id)
-
-      # Assert
-      must_respond_with :success 
-     end
-
-  end
-
-  describe "update" do
-    # Your tests go here
   end
 
   describe "destroy" do
-    # Your tests go here
+    it "destroys the trip instance in db when trip exists, then redirects" do
+      # Arrange
+      # Ensure there is an existing driver saved
+      id = @trip.id
+
+      # Act-Assert
+      # Ensure that there is a change of -1 in Driver.count
+      expect {
+        delete trip_path(id)
+      }.must_change "Trip.count", -1
+
+      # Assert
+      # Check that the controller redirects
+      deleted_trip = Trip.find_by(id: id)
+
+      expect(deleted_trip).must_be_nil
+
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
+
+    it "does not change the db when the trip does not exist, then responds with redirect" do
+      # Arrange
+      # Ensure there is an invalid id that points to no driver
+      @trip.destroy
+
+      # Act-Assert
+      # Ensure that there is no change in Driver.count
+      expect {
+        delete trip_path(@trip.id)
+      }.wont_differ "Trip.count"
+
+      # Assert
+      # Check that the controller responds or redirects with whatever your group decides
+      must_respond_with :redirect
+      must_redirect_to root_path
+    end
   end
 end
