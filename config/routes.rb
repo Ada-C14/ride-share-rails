186c6ae85cacb8a81a730e071b284c26d24012b2
Hanh Solo <minhhanhnyc@gmail.com>
2020-11-18 10:55:14 -0800

    Ignore .env file

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
-  resources :drivers do
-    resources :trips, only: [:index, :new]
-  end
+  # We do not need these nested routes because we don't use them. 
+  # resources :drivers do
+  #   resources :trips, only: [:index, :new]
+  # end 
 
   resources :passengers do
     resources :trips, only: [:create]
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
 
   resources :drivers
   resources :passengers
-  resources :trips
+  resources :trips, except: [:new, :create]# we create trip through passenger. 
 
   root to: 'homepages#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
